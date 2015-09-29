ba0a63d1d96b9d2a3e90813299ade1371cb4c8ba
Guido Novati <novatig@gmail.com>
2015-09-29 18:42:34 +0200

    Updated some backups

---
+++ source/ANNTests/launch.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export OMP_NUM_THREADS=1
+export LD_LIBRARY_PATH=/Users/laskariangeliki/Documents/tbb40_297oss/lib/:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/Users/laskariangeliki/Documents/tbb40_297oss/lib/:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/home/novatig/armadillo/usr/lib64/:$LD_LIBRARY_PATH
+
+./rl
+
+
+
+
+
+
+
+
+
