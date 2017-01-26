272b5a7580373d5763f22360b47abf927ac1e79d
novatig <novatig@ethz.ch>
2017-01-26 10:33:41 +0100

    updated launch scripts and settings for 3D 2 fish on daint

--- apps/Daint_2Fish1act_3D/settings2Stefans.sh
+++ apps/Daint_2Fish1act_3D/settings2Stefans.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 OPTIONS=
 OPTIONS+=" -nActions 1"
-OPTIONS+=" -bpdx 128 -bpdy 64 -bpdz 16"
+OPTIONS+=" -bpdx 64 -bpdy 32 -bpdz 16"
 OPTIONS+=" -2Ddump 1 -restart 0"
 OPTIONS+=" -nprocsz 1"
 OPTIONS+=" -CFL 0.1"
-OPTIONS+=" -length 0.15"
+OPTIONS+=" -length 0.2"
 OPTIONS+=" -lambda 1e5"
-OPTIONS+=" -nu 0.0000045"
+OPTIONS+=" -nu 0.000008"
 OPTIONS+=" -tend 80"
