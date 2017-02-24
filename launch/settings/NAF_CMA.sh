efcbcf1b8aae7537532494c43046a8112a789c2f
Guido Novati <novatig@ethz.ch>
2017-02-24 14:35:17 +0100

    update to settings file: new feature comments

--- launch/settings/NAF_CMA.sh
+++ launch/settings/NAF_CMA.sh
@@ -1,16 +1,30 @@
 SETTINGS=
-SETTINGS+=" --fileSamp none"
-SETTINGS+=" --gamma 0.99"
-SETTINGS+=" --learnrate 0.001"
+#file that stores every observtion (log of states and actions)
+#if none then no output
+SETTINGS+=" --fileSamp none" 
+#discount factor in RL
+SETTINGS+=" --gamma 0.9"
+#network update learning rate
+SETTINGS+=" --learnrate 0.0001"
+#size of network layers
 SETTINGS+=" --nnl1 48"
-SETTINGS+=" --nnl2 12"
+SETTINGS+=" --nnl2 24"
 SETTINGS+=" --nnl3 0"
-SETTINGS+=" --nnType 1"
-SETTINGS+=" --nnL 0.0001"
-SETTINGS+=" --greedyeps 0.01"
+#0 means feed forward neural nets
+#1 means LSTM
+#subject to changes
+SETTINGS+=" --nnType 0"
+# L2 regularization of the weights 
+SETTINGS+=" --nnL 0.00"
+#chance of taking random actions
+SETTINGS+=" --greedyeps 0.1"
+#whether you are training a policy or testing an already trained network
 SETTINGS+=" --bTrain 1"
-SETTINGS+=" --rType 0"
-SETTINGS+=" --senses 0"
+#RL algorithm: NAF, DPG are continuous actions, NFQ (also accepted DQN) is for discrete actions
 SETTINGS+=" --learn NAF"
-SETTINGS+=" --dqnBatch 200"
-SETTINGS+=" --dqnT 0.0001"
+#batch size for network gradients compute
+SETTINGS+=" --dqnBatch 48"
+#lag of target network. 
+#if >1 then weights are copied every dqnT grad descent steps
+#if <1 then every step the target weights are updated as dqnT * w_Target + (1-dqnT)*w
+SETTINGS+=" --dqnT 0.001"
