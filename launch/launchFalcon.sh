1d6db65b74589789d76c63249727a605cb1597d4
Guido Novati <novatig@ethz.ch>
2017-03-16 16:41:49 +0100

    minor comments

--- launch/launchFalcon.sh
+++ launch/launchFalcon.sh
@@ -2,9 +2,9 @@
 EXECNAME=rl
 RUNFOLDER=$1
 NTHREADS=$2 # TEMP: here used as nthreads
-NTASK=$3
-APP=$4
-SETTINGSNAME=$5
+NTASK=$3 #at least 2 to have a slave
+APP=$4 # name of a folder in the ../apps/ folder, which contains the executable and factory file
+SETTINGSNAME=$5 #settings script, usually found in ./settings/
 
 BASEPATH="../runs/"
 mkdir -p ${BASEPATH}${RUNFOLDER}
@@ -16,8 +16,11 @@ if [ $# -gt 5 ] ; then
 fi
 
 #this must handle all app-side setup (as well as copying the factory)
-source ../apps/${APP}/setup.sh ${BASEPATH}${RUNFOLDER}
-
+if [ "${APP:0:8}" == "../apps/" ]; then
+	source ${APP}/setup.sh ${BASEPATH}${RUNFOLDER}
+else
+	source ../apps/${APP}/setup.sh ${BASEPATH}${RUNFOLDER}
+fi
 cp ../makefiles/${EXECNAME} ${BASEPATH}${RUNFOLDER}/exec
 cp ${SETTINGSNAME} ${BASEPATH}${RUNFOLDER}/settings.sh
 cp ${SETTINGSNAME} ${BASEPATH}${RUNFOLDER}/policy_settings.sh
