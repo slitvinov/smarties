c428a839b14546ca3b9006bb7c526d81693a8667
Guido Novati <novatig@gmail.com>
2017-04-08 01:48:13 +0200

    minor

--- launch/settings/cart_pole.sh
+++ launch/settings/cart_pole.sh
@@ -6,16 +6,16 @@ SETTINGS+=" --fileSamp history.txt"
 #discount factor in RL
 #the closer to 1 it is, the harder it is to learn
 #but, the agent might find better long-term strategies
-SETTINGS+=" --gamma 0.90"
+SETTINGS+=" --gamma 0.9"
 
 #network update learning rate
-SETTINGS+=" --learnrate 0.00001"
+SETTINGS+=" --learnrate 0.0001"
 
 #size of network layers
 SETTINGS+=" --nnl1 16"
 SETTINGS+=" --nnl2 32"
 SETTINGS+=" --nnl3 16"
-SETTINGS+=" --nnl4 8"
+SETTINGS+=" --nnl4 16"
 
 #0 means feed forward neural nets
 #1 means LSTM
@@ -23,7 +23,7 @@ SETTINGS+=" --nnl4 8"
 SETTINGS+=" --nnType 0"
 
 # L2 regularization of the weights
-SETTINGS+=" --nnL 0.00"
+SETTINGS+=" --nnL 0.0001"
 
 #chance of taking random actions
 SETTINGS+=" --greedyeps 0.01"
@@ -52,7 +52,7 @@ SETTINGS+=" --maxTotSeqNum 1000"
 #batch size for network gradients compute
 SETTINGS+=" --dqnBatch 8"
 
-SETTINGS+=" --epsAnneal 100000" 
+SETTINGS+=" --epsAnneal 10000" 
 #lag of target network.
 #- if >1 (ie 1000) then weights are copied every dqnT grad descent steps
 #- if <1 (ie .001) then every step the target weights are updated as dqnT * w_Target + (1-dqnT)*w
