0767504eece5dbdba142419e83ce346e6b122a55
Yannick <huberya@student.ethz.ch>
2017-04-20 18:24:05 +0200

    breakout

--- apps/alebot/breakout/launch_ale.sh
+++ apps/alebot/breakout/launch_ale.sh
@@ -1,4 +1,4 @@
 SOCK=$1
 
 export LD_LIBRARY_PATH=${HOME}/smarties/apps/alebot/Arcade-Learning-Environment/:$LD_LIBRARY_PATH
-../alebot ${SOCK}
+../alebot ${SOCK} ${HOME}/ROMS/Breakout.bin
--- apps/alebot/setup.sh
+++
@@ -1,3 +0,0 @@
-cp ../apps/alebot/factory ${BASEPATH}${RUNFOLDER}/factory
-cp ../apps/alebot/launch_ale.sh ${BASEPATH}${RUNFOLDER}/launchSim.sh
-cp ../apps/alebot/alebot ${BASEPATH}${RUNFOLDER}/
