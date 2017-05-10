9086c1b31667e15e24878ad90bd7324d6f1130c5
Yannick <huberya@student.ethz.ch>
2017-05-10 13:47:10 +0200

    next

--- apps/openai/openaibot.py
+++ apps/openai/openaibot.py
@@ -37,24 +37,28 @@ while True:
 	agent_id=0 #assume only 1 exist for now
 	reward=0
 	observation=np.zeros_like(env.reset(), dtype=np.float64)
+	state=np.zeros(obeservation.size+3)
 	for i_episode in range(int(float(sys.argv[3]))):
 		print("env.reset()")
 		observation = env.reset()
 		for t in range(int(float(sys.argv[4]))):
-			temparr=np.array([agent_id, status])
-			state=np.concatenate((temparr, observation),axis=0)
-			state.append(reward)
+			state[0]=agent_id
+			state[1]=status
+			state[2:observation.size+2]
+			state[observation.size+2]=reward
 			conn.send(state.tobytes())
 			status[0]=0
 			buf=conn.recv(nActions*8)
 			action=np.frombuffer(buf,dtype=np.float64)
-			observation, reward_temp, done, info = env.step(action)
-			reward[0]=rewart_temp
+			observation, reward, done, info = env.step(action)
 			if done:
 				print("Episode finished after {} timesteps".format(t+1))
 				break
 		info[0]=2
-		state=np.concatenate((agent_id, status, observation, reward),axis=0)
+		state[0]=agent_id
+		state[1]=status
+		state[2:observation.size+2]
+		state[observation.size+2]=reward
 		conn.send(state.tobytes())
 		info[0]=1
 		reward[0]=0
