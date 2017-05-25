06b969de95926993ec4a25a4fbdf44fce05b0a37
Guido Novati <novatig@ethz.ch>
2017-05-25 15:53:20 -0400

    minor containers

---
+++ source/Bund.h
@@ -0,0 +1,141 @@
+/*
+ *  Settings.h
+ *  rl
+ *
+ *  Created by Guido Novati on 02.05.16.
+ *  Copyright 2013 ETH Zurich. All rights reserved.
+ *
+ */
+#pragma once
+
+using namespace std;
+
+//#define __posDef_layers_
+#include <vector>
+#include <cassert>
+#include <sstream>
+#include <cstring>
+#include <utility>
+#include <limits>
+#include <cmath>
+#include <immintrin.h>
+
+#include <omp.h>
+#include <mpi.h>
+//#define __vec_width__ 32
+#define __vec_width__ 8
+
+#define _BPTT_
+#define _dumpNet_
+
+typedef double Real;
+#define MPI_VALUE_TYPE MPI_DOUBLE
+static const int simdWidth = __vec_width__/sizeof(Real);
+
+template <typename T>
+void _myfree(T *const& ptr)
+{
+  if(ptr == nullptr) return;
+  free(ptr);
+}
+
+template <typename T>
+void _allocateQuick(T *const& ptr, const int size)
+{
+    const int sizeSIMD=std::ceil(size/(Real)simdWidth)*simdWidth*sizeof(Real);
+    posix_memalign((void **)& ptr, __vec_width__, sizeSIMD);
+}
+
+template <typename T>
+void _allocateClean(T *const& ptr, const int size)
+{
+    const int sizeSIMD=std::ceil(size/(Real)simdWidth)*simdWidth*sizeof(Real);
+    posix_memalign((void **)& ptr, __vec_width__, sizeSIMD);
+    memset(ptr, 0, sizeSIMD);
+}
+
+template <typename T>
+void _dispose_object(T *& ptr)
+{
+    if(ptr == nullptr) return;
+    delete ptr;
+    ptr=nullptr;
+}
+
+template <typename T>
+void _dispose_object(T *const& ptr)
+{
+    if(ptr == nullptr) return;
+    delete ptr;
+}
+
+template <typename T>
+inline string printVec(const vector<T> vals)
+{
+  std::ostringstream o;
+  for (int i=0; i<vals.size(); i++) o << " " << vals[i];
+  return o.str();
+}
+
+inline bool nonZero(const Real vals)
+{
+  return std::fabs(vals) > std::numeric_limits<Real>::epsilon();
+}
+
+inline bool positive(const Real vals)
+{
+  return vals > std::numeric_limits<Real>::epsilon();
+}
+
+inline void setVecMean(vector<Real>& vals)
+{
+   assert(vals.size()>1);
+	Real mean = 0;
+	for (unsigned int i=1; i<vals.size(); i++) //assume 0 is empty
+		mean += vals[i];
+	mean /= (Real)(vals.size()-1);
+	for (unsigned int i=0; i<vals.size(); i++)
+		vals[i] = mean;
+}
+
+inline void statsVector(vector<vector<Real>>& sum, vector<vector<Real>>& sqr,
+  vector<Real>& cnt)
+{
+   assert(sum.size()>1);
+  assert(sum.size() == cnt.size() && sqr.size() == cnt.size());
+
+  for (unsigned int i=0; i<sum[0].size(); i++)
+    sum[0][i] = sqr[0][i] = 0;
+  cnt[0] = 0;
+
+  for (unsigned int i=1; i<sum.size(); i++) {
+    cnt[0] += cnt[i]; cnt[i] = 0;
+    for (unsigned int j=0; j<sum[0].size(); j++)
+    {
+      sum[0][j] += sum[i][j]; sum[i][j] = 0;
+      sqr[0][j] += sqr[i][j]; sqr[i][j] = 0;
+    }
+  }
+  cnt[0] = std::max(2.2e-16, cnt[0]);
+  for (unsigned int j=0; j<sum[0].size(); j++)
+  {
+    sqr[0][j] = std::sqrt((sqr[0][j]-sum[0][j]*sum[0][j]/cnt[0])/cnt[0]);
+    sum[0][j] /= cnt[0];
+  }
+}
+
+inline void statsGrad(vector<Real>& sum, vector<Real>& sqr, Real& cnt, vector<Real> grad)
+{
+  assert(sum.size() == grad.size() && sqr.size() == grad.size());
+  cnt += 1;
+  for (unsigned int i=0; i<grad.size(); i++) {
+    sum[i] += grad[i];
+    sqr[i] += grad[i]*grad[i];
+  }
+}
+
+inline void Lpenalization(Real* const weights, const int start, const int N, const Real lambda)
+{
+  for (int i=start; i<start+N; i++) weights[i]+= (weights[i]<0 ? lambda : -lambda);
+  //for (int i=start; i<start+N; i++) weights[i]-= weights[i]*lambda;
+}
---
+++ source/allLearners.h
@@ -0,0 +1,62 @@
+/*
+ *  main.cpp
+ *  rl
+ *
+ *  Created by Dmitry Alexeev on 02.05.13.
+ *  Copyright 2013 ETH Zurich. All rights reserved.
+ *
+ */
+
+#include "Learners/Learner.h"
+#include "Learners/NFQ.h"
+#include "Learners/NAF.h"
+#include "Learners/DPG.h"
+#include "Learners/RACER.h"
+#include "Learners/DACER.h"
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fstream>
+
+Learner* createLearner(MPI_Comm mastersComm, Environment*const env, Settings&settings)
+{
+	if(settings.learner=="DQ" || settings.learner=="DQN" || settings.learner=="NFQ") {
+		settings.nnInputs = env->sI.dimUsed*(1+settings.dqnAppendS);
+		settings.nnOutputs = env->aI.maxLabel;
+		return new NFQ(mastersComm, env, settings);
+	}
+	else if (settings.learner == "RACER") {
+		settings.nnInputs = env->sI.dimUsed*(1+settings.dqnAppendS);
+		settings.nnOutputs = RACER::getnOutputs(env->aI.dim);
+		settings.separateOutputs = true; //else it does not really work
+		return new RACER(mastersComm, env, settings);
+	}
+	else if (settings.learner == "DACER") {
+		settings.nnInputs = env->sI.dimUsed*(1+settings.dqnAppendS);
+		const int nA = env->aI.maxLabel;
+		printf("Read %d outputs\n",nA);
+		settings.nnOutputs = DACER::getnOutputs(nA);
+		settings.separateOutputs = true; //else it does not really work
+		return new DACER(mastersComm, env, settings);
+	}
+	else if (settings.learner == "NA" || settings.learner == "NAF") {
+		settings.nnInputs = env->sI.dimUsed*(1+settings.dqnAppendS);
+		const int nA = env->aI.dim;
+		const int nL = (nA*nA+nA)/2;
+		settings.nnOutputs = 1+nL+nA;
+		settings.separateOutputs = true; //else it does not really work
+		return new NAF(mastersComm, env, settings);
+	}
+	else if (settings.learner == "DP" || settings.learner == "DPG") {
+		settings.nnInputs = env->sI.dimUsed*(1+settings.dqnAppendS) + env->aI.dim;
+		settings.nnOutputs = 1;
+		return new DPG(mastersComm, env, settings);
+	} else die("Learning algorithm not recognized\n");
+	assert(false);
+	return new NFQ(mastersComm, env, settings); //fake, to silence warnings
+}
