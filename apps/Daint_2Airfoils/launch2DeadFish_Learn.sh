d5a9ed3a24ed7dbc7513e0a023a5a8ec5c55dec2
Iveta Rott <rotti@student.ethz.ch>
2017-06-29 11:52:08 +0200

    Daint_2Airfoils folder

---
+++ apps/Daint_2Airfoils/launch2DeadFish_Learn.sh
@@ -0,0 +1,44 @@
+SOCK=$1
+PROGNAME=hyperion
+SETTINGSNAME=deadSettings.sh
+EXECNAME=LearningSim
+NTHREADS=12
+
+MYNAME=`whoami`
+BASEPATH="./"
+
+RESTART=" -restart 0"
+mkdir -p ${BASEPATH}${EXECNAME}
+source ../$SETTINGSNAME
+
+factoryFile=`echo $SETTINGS | awk 'BEGIN{FS="-factory "} {print $2}' | awk '{print $1}'`
+if [ -n "$factoryFile" ]; then
+if [ ! -f $factoryFile ];then
+echo "factoryFile "$factoryFile" not found! - exiting"
+exit -1
+fi
+echo "factoryFile detected: "$factoryFile
+fi
+
+#cp $HOME/MRAGapps/IF2D_ROCKS/launch/restart_learn/* ${BASEPATH}${EXECNAME}/
+cp ../launchSim.sh ${BASEPATH}${EXECNAME}/
+cp ../$SETTINGSNAME ${BASEPATH}${EXECNAME}/
+cp ../hyperion ${BASEPATH}${EXECNAME}/
+
+if [ -n "$factoryFile" ]; then
+cp $factoryFile ${BASEPATH}${EXECNAME}/
+fi
+env > ${BASEPATH}${EXECNAME}/environment.log
+
+
+SETTINGS+=" -nthreads ${NTHREADS}"
+SETTINGS+=" -sock ${SOCK}"
+OPTIONS=${SETTINGS}${RESTART}
+
+cd ${BASEPATH}${EXECNAME}
+export LD_LIBRARY_PATH=${HOME}/2d-treecodes/:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=${HOME}/tbb2017/build/linux_intel64_gcc_cc5.3.0_libc2.19_kernel3.12.60_release:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/users/sverma/usr/VTK-7.1.1/lib/:$LD_LIBRARY_PATH
+export OMP_NUM_THREADS=${NTHREADS}
+
+./hyperion ${OPTIONS}
