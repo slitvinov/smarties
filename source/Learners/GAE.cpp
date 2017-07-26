8c932b7c6b42c819a2fe8ed1e2294e39377053bf
Guido Novati <novatig@ethz.ch>
2017-07-26 15:11:01 -0400

    bugfixz

--- source/Learners/GAE.cpp
+++ source/Learners/GAE.cpp
@@ -148,7 +148,7 @@ void GAE::select(const int agentId, const Agent& agent)
   agent.a->set(act);
   #endif
 
-  data->writeData(agentId, a, beta);
+  data->writeData(agentId, agent, beta);
   if(thrid==0 && profiler_ext != nullptr) profiler_ext->stop_start("COMM");
   if(thrid==1) profiler->pop_stop();
   //data->passData(agentId, agent, beta);
