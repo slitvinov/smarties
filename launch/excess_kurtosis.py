237489017767f64b349332ac79d5d5f5c03fc491
Guido Novati <novatig@gmail.com>
2018-02-13 23:32:46 +0100

    minor and script update

--- launch/excess_kurtosis.py
+++ launch/excess_kurtosis.py
@@ -3,12 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 FILE =     sys.argv[1]
 M    = int(sys.argv[2])
-ICOL = int(sys.argv[3])
+#ICOL = int(sys.argv[3])
 
 
 #np.savetxt(sys.stdout, np.fromfile(sys.argv[1], dtype='i4').reshape(2,10).transpose())
 DATA = np.fromfile(FILE, dtype=np.float32)
-DATA = DATA.reshape([DATA.size//4, 4])
+#DATA = DATA.reshape([DATA.size//2, 2])
 N = DATA.shape[0]
 norm1 = M ** 0.50
 norm2 = M ** 0.25
@@ -16,7 +16,7 @@ for i in range(0, (N//M)*M, M):
   start = i
   end = start + M
   #x = np.random.normal(0, 1, M) # debug, must print (0, 1)
-  x = DATA[start:end, ICOL]
+  x = DATA[start:end] #, ICOL]
   mean_1 = np.mean(x)
   stdv_1 = np.std(x)
   numer =  np.sum( ((x-mean_1)/norm2)**4 )  
