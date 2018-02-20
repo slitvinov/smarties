e3e5589d4f039fdfe448e63ff4dfc8cfd2260171
Guido Novati <novatig@ethz.ch>
2018-02-20 17:44:36 +0100

    update kurtosis postprocess file

--- launch/random_kurtosis_driver_script.sh
+++ launch/random_kurtosis_driver_script.sh
@@ -1,17 +1,23 @@
 
 BASEDIR="/scratch/snx3000/novatig/smarties/"
-PREFIX="_chosenParam_11_GAUS_S1_"
+PREFIX="_chosenParam_12_GAUS_"
 FNAME="/grads_dist.raw"
 
 for ENV in "walker" "spider" "reachr" "humanw" "cheeta" "hopper" "swimmr" "dblpnd"; do
-for R in "2"; do
-for N in "131072" "262144" "524288"; do
-for D in "0.05";
+#for R in "2"; do
+for R in "5"; do
+#for N in "131072" "262144" "524288"; do
+for N in "262144"; do
+for D in "0.2"; do
+for B in "256"; do
+for O in "1.0"; do
 
-RUN=${BASEDIR}${ENV}${PREFIX}_R${R}_N${N}_D${D}_TRICK0_TRIAL
+RUN=${BASEDIR}${ENV}${PREFIX}_R${R}_N${N}_D${D}_B${B}_O${O}_TRIAL
 echo
 python excess_kurtosis.py 16 ${RUN}1${FNAME} ${RUN}2${FNAME} ${RUN}3${FNAME}
 
+done
+done
 done
 done
 done
