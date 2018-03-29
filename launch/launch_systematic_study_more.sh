ec6b853b8336e2cd99a99cc3537758cd42da0ab5
novatig <novatig@ethz.ch>
2018-03-29 17:03:05 +0200

    added minor script

---
+++ launch/launch_systematic_study_more.sh
@@ -0,0 +1,41 @@
+COMMNAME=anneal_ETA0.00003
+
+for EXPTYPE in "GAUS"; do
+for NEXPERTS in "1"; do
+for SKIPA in 1; do
+
+#make -C ../makefiles clean
+#make -C ../makefiles config=fit -j raceskip=$SKIPA exp=$EXPTYPE nexp=$NEXPERTS
+
+for BUFFSIZE in "524288"; do
+for IMPSAMPR in "4"; do
+
+for DKLPARAM in "0.1"; do
+for BATCHNUM in "256"; do
+for EPERSTEP in "1.0"; do
+for RUNTRIAL in "1" "2" "3" "4" "5"; do
+
+POSTFIX=${COMMNAME}_R${IMPSAMPR}_N${BUFFSIZE}_D${DKLPARAM}_B${BATCHNUM}_O${EPERSTEP}_TRIAL${RUNTRIAL}
+NMASTERS=1
+#source launchDaint_openai.sh standu_${POSTFIX} HumanoidStandup-v2        settings/settings_bench_args.sh
+source launchDaint_openai.sh humanw_${POSTFIX} Humanoid-v2               settings/settings_bench_args.sh
+#source launchDaint_openai.sh invpnd_${POSTFIX} InvertedPendulum-v2       settings/settings_bench_args.sh
+source launchDaint_openai.sh spider_${POSTFIX} Ant-v2                    settings/settings_bench_args.sh
+#source launchDaint_openai.sh dblpnd_${POSTFIX} InvertedDoublePendulum-v2 settings/settings_bench_args.sh
+source launchDaint_openai.sh walker_${POSTFIX} Walker2d-v2               settings/settings_bench_args.sh
+source launchDaint_openai.sh cheeta_${POSTFIX} HalfCheetah-v2            settings/settings_bench_args.sh
+source launchDaint_openai.sh swimmr_${POSTFIX} Swimmer-v2                settings/settings_bench_args.sh
+#source launchDaint_openai.sh hopper_${POSTFIX} Hopper-v2                 settings/settings_bench_args.sh
+#source launchDaint_openai.sh reachr_${POSTFIX} Reacher-v2                settings/settings_bench_args.sh
+
+done #RUNTRIAL
+done #EPERSTEP
+done #BATCHNUM
+done #BATCHNUM
+
+done
+done
+
+done
+done
+done
