3838d5db37b944f9e5c4cff984ff1dcd459181bf
novatig <novatig@ethz.ch>
2018-04-06 21:38:49 +0200

    script update

---
+++ launch/launch_systematic_study_DPG.sh
@@ -0,0 +1,47 @@
+COMMNAME=DPG_01
+
+for EXPTYPE in "GAUS"; do
+for NEXPERTS in "1"; do
+for SKIPA in "1"; do
+
+make -C ../makefiles clean
+make -C ../makefiles config=fit -j raceskip=$SKIPA exp=$EXPTYPE nexp=$NEXPERTS
+
+for BUFFSIZE in "262144" "524288" "1048576"; do
+#for IMPSAMPR in "2" "3" "4" "5"; do
+#for IMPSAMPR in "2" "3" "4" "5"; do
+for IMPSAMPR in "4" "0"; do
+#for DKLPARAM in "0.05" "0.1" "0.2"; do
+
+for DKLPARAM in "0.10"; do
+for BATCHNUM in "128"; do
+for EPERSTEP in "1"; do
+
+for RUNTRIAL in "1" "2" "3" "4" "5"; do
+
+POSTFIX=${COMMNAME}_R${IMPSAMPR}_N${BUFFSIZE}_D${DKLPARAM}_TRIAL${RUNTRIAL}
+NMASTERS=1
+echo $POSTFIX
+#source launchDaint_openai.sh standu_${POSTFIX} HumanoidStandup-v2        settings/settings_bench_DPG.sh
+#source launchDaint_openai.sh humanw_${POSTFIX} Humanoid-v2               settings/settings_bench_DPG.sh
+#source launchDaint_openai.sh invpnd_${POSTFIX} InvertedPendulum-v2       settings/settings_bench_DPG.sh
+source launchDaint_openai.sh spider_${POSTFIX} Ant-v2                    settings/settings_bench_DPG.sh
+#source launchDaint_openai.sh dblpnd_${POSTFIX} InvertedDoublePendulum-v2 settings/settings_bench_DPG.sh
+source launchDaint_openai.sh walker_${POSTFIX} Walker2d-v2               settings/settings_bench_DPG.sh
+source launchDaint_openai.sh cheeta_${POSTFIX} HalfCheetah-v2            settings/settings_bench_DPG.sh
+source launchDaint_openai.sh swimmr_${POSTFIX} Swimmer-v2                settings/settings_bench_DPG.sh
+source launchDaint_openai.sh hopper_${POSTFIX} Hopper-v2                 settings/settings_bench_DPG.sh
+#source launchDaint_openai.sh reachr_${POSTFIX} Reacher-v2                settings/settings_bench_DPG.sh
+
+done
+
+done
+done
+
+done
+done
+done
+
+done
+done
+done
--- launch/random_kurtosis_driver_script.sh
+++ launch/random_kurtosis_driver_script.sh
@@ -1,9 +1,9 @@
 
-BASEDIR="/scratch/snx3000/novatig/smarties/"
-PREFIX="_chosenParam_12_GAUS_"
+BASEDIR="/scratch/snx3000/novatig/smarties"
+PREFIX="chosenParam_12_GAUS"
 FNAME="/grads_dist.raw"
 
-for ENV in "walker" "spider" "reachr" "humanw" "cheeta" "hopper" "swimmr" "dblpnd"; do
+for ENV in "walker" "standu" "spider" "reachr" "humanw" "cheeta" "hopper" "swimmr" "dblpnd"; do
 #for R in "2"; do
 for R in "5"; do
 #for N in "131072" "262144" "524288"; do
@@ -12,7 +12,7 @@ for D in "0.2"; do
 for B in "256"; do
 for O in "1.0"; do
 
-RUN=${BASEDIR}${ENV}${PREFIX}_R${R}_N${N}_D${D}_B${B}_O${O}_TRIAL
+RUN=${BASEDIR}/${ENV}_${PREFIX}_R${R}_N${N}_D${D}_B${B}_O${O}_TRIAL
 echo
 python excess_kurtosis.py 16 ${RUN}1${FNAME} ${RUN}2${FNAME} ${RUN}3${FNAME}
 
