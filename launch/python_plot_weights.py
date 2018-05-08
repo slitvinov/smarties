7eb5288a42f56ee73fd564ae3ec20397d20a86c1
Guido Novati <novatig@ethz.ch>
2018-05-08 19:47:13 +0200

    additional info looked at in weight vector

--- launch/python_plot_weights.py
+++ launch/python_plot_weights.py
@@ -18,12 +18,22 @@ FILE=    sys.argv[1]
 W  = np.fromfile(FILE+"weights.raw", dtype=np.float64)
 M1 = np.fromfile(FILE +"1stMom.raw", dtype=np.float64)
 M2 = np.fromfile(FILE +"2ndMom.raw", dtype=np.float64)
+INDS = np.where(M2>1e-16)
+#INDS = np.arange(64768,73984)
+#INDS = np.reshape(INDS, [128,72])
+#INDS = INDS[:, 1:36]
+#INDS = INDS[:, 37:54]
+#INDS = INDS[:, 54:71]
+#INDS = INDS[:]
+W  = W[INDS];
+M1 = M1[INDS];
+M2 = M2[INDS];
 assert(not np.isnan(W).any())
 assert(not np.isnan(M1).any())
 assert(not np.isnan(M2).any())
 
 plt.subplot(221)
-plt.semilogy(1e-6 + abs(W),'b.')
+plt.semilogy(1e-7 + abs(W),'b.')
 plt.title('Weights')
 #plt.plot(abs(W),'b.')
 
@@ -32,7 +42,11 @@ plt.semilogy(abs(M1)/(1e-7 + np.sqrt(M2)) + 1e-6,'g.')
 plt.title('Abs Grad')
 
 plt.subplot(223)
-plt.semilogy(abs(M1) + 1e-6,'k.')
+#plt.semilogy(abs(W)*1e-07 + 1e-12,'r.',alpha=0.2)
+plt.semilogy((abs(W)*1e-8)/np.maximum(abs(M1),np.sqrt(M2)),'k.')
+#plt.semilogy((abs(W)*1e-8)/(np.sqrt(M2)),'k.')
+#plt.semilogy((abs(W)*1e-8)/(abs(M1)),'k.')
+#plt.semilogy(abs(M1) + 1e-8,'k.')
 plt.title('Abs M1')
 
 plt.subplot(224)
