1fd1cd6e8fed4984d89ce5f8a7cf02c77d2db0df
novatig <novatig@ethz.ch>
2018-05-08 19:48:47 +0200

    added the wrong file

--- launch/python_plot_weights.py
+++ launch/python_plot_weights.py
@@ -18,25 +18,38 @@ FILE=    sys.argv[1]
 W  = np.fromfile(FILE+"weights.raw", dtype=np.float64)
 M1 = np.fromfile(FILE +"1stMom.raw", dtype=np.float64)
 M2 = np.fromfile(FILE +"2ndMom.raw", dtype=np.float64)
+INDS = np.where(M2>1e-16)
+W  = W[INDS];
+M1 = M1[INDS];
+M2 = M2[INDS];
 assert(not np.isnan(W).any())
 assert(not np.isnan(M1).any())
 assert(not np.isnan(M2).any())
 
-plt.subplot(221)
+plt.subplot(231)
 plt.semilogy(1e-6 + abs(W),'b.')
 plt.title('Weights')
 #plt.plot(abs(W),'b.')
 
-plt.subplot(222)
+plt.subplot(232)
+plt.semilogy(abs(M1) + 1e-12,'k.')
+plt.title('Abs M1')
+
+plt.subplot(233)
+plt.semilogy(1e-6 + np.sqrt(M2), 'g.')
+#plt.semilogy(abs(M)/S,'g.')
+plt.title('Sqrt M2')
+
+plt.subplot(234)
 plt.semilogy(abs(M1)/(1e-7 + np.sqrt(M2)) + 1e-6,'g.')
 plt.title('Abs Grad')
 
-plt.subplot(223)
-plt.semilogy(abs(M1) + 1e-6,'k.')
-plt.title('Abs M1')
+plt.subplot(235)
+plt.semilogy((abs(W)*1e-6)/(np.sqrt(M2)),'k.')
 
-plt.subplot(224)
-plt.semilogy(1e-6 + np.sqrt(M2), 'g.')
-plt.title('Sqrt M2')
-#plt.semilogy(abs(M)/S,'g.')
+
+plt.subplot(236)
+#plt.semilogy(abs(W)*1.19209e-07 + 1e-12,'r.',alpha=0.2)
+plt.semilogy((abs(W)*1e-6)/(abs(M1)),'k.')
+
 plt.show()
