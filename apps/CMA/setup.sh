d2a6ec30ef82b35ff94ec22efcd58fc352cc882f
Guido Novati <novatig@ethz.ch>
2018-06-13 20:31:27 +0200

    setup to run CMA: ./launch.sh test_CMA_00 4 CMA settings/settings_RACER_RNN.sh

---
+++ apps/CMA/setup.sh
@@ -0,0 +1,4 @@
+cp ../apps/CMA/engine_cmaes ${BASEPATH}${RUNFOLDER}/
+cp ../apps/CMA/cmaes_initials.par ${BASEPATH}${RUNFOLDER}/
+cp ../apps/CMA/cmaes_signals.par ${BASEPATH}${RUNFOLDER}/
+cp ../apps/CMA/launch.sh ${BASEPATH}${RUNFOLDER}/launchSim.sh
