aa96d18718520ecc0c800d6932b0d21ff7eeb4c2
Guido Novati <novatig@gmail.com>
2018-07-31 19:11:49 +0200

    minor

--- apps/blowfish/launchSim.sh
+++ apps/blowfish/launchSim.sh
@@ -1,7 +1,7 @@
 SOCKET=$1
-export OMP_NUM_THREADS=12
+export OMP_NUM_THREADS=6
 OPTIONS="-bpdx 8 -bpdy 8 -tdump 0.0 -shape blowfish -radius 0.16 -nu 0.0004 -tend 0 -rhoS 0.5"
-
+OPTIONS+=" -Socket ${SOCKET}"
 export LD_LIBRARY_PATH=/cluster/home/novatig/VTK-7.1.0/Build/lib/:$LD_LIBRARY_PATH
 
 echo $OPTIONS > opts.txt
