2a701c8d7f66973b190ec4ece7341c8d6090fc12
Guido Novati <novatig@gmail.com>
2018-08-06 11:04:50 +0200

    minor

--- apps/leadFollow/runArguments.sh
+++ apps/leadFollow/runArguments.sh
@@ -1,1 +1,1 @@
-../launchSim.sh -muteAll 1 -verbose 0 -bpdx 16 -bpdy 8 -tdump 0.5 -nu 0.000008 -tend 0 -shapes 'stefanfish L=0.2 xpos=0.2 bFixed=1 pid=1 \nstefanfish L=0.2 xpos=0.5'
+../launchSim.sh -muteAll 1 -verbose 0 -bpdx 16 -bpdy 8 -tdump 0 -nu 0.000008 -tend 0 -shapes 'stefanfish L=0.2 xpos=0.2 bFixed=1 pid=1 \nstefanfish L=0.2 xpos=0.5'
