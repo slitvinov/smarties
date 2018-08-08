d639127a91c4db461344d33099addd723da386b5
Guido Novati <novatig@ethz.ch>
2018-08-08 14:10:38 +0200

    mostly an update for CUP2D compilation

--- apps/blowfish_varRes/runArguments01.sh
+++ apps/blowfish_varRes/runArguments01.sh
@@ -1,1 +1,1 @@
-../launchSim.sh -muteAll 1 -bpdx 8 -bpdy 8 -tdump 0.5 -nu 0.0004 -tend 0 -shapes 'blowfish L=0.16 bFixed=1'
+../launchSim.sh -bFreeSpace 0 -muteAll 1 -bpdx 8 -bpdy 8 -tdump 0.5 -nu 0.0004 -tend 0 -shapes 'blowfish L=0.16 bFixed=1'
--- apps/blowfish_varRes/runArguments02.sh
+++ apps/blowfish_varRes/runArguments02.sh
@@ -1,1 +1,1 @@
-../launchSim.sh -muteAll 1 -bpdx 16 -bpdy 16 -tdump 0.5 -nu 0.0004 -tend 0 -shapes 'blowfish L=0.16 bFixed=1'
+../launchSim.sh -bFreeSpace 0 -muteAll 1 -bpdx 16 -bpdy 16 -tdump 0.5 -nu 0.0004 -tend 0 -shapes 'blowfish L=0.16 bFixed=1'
