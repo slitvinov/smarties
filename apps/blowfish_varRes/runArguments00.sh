16feae0ac677e14281316caa9b70f53e27af23fe
Guido Novati <novatig@ethz.ch>
2018-08-10 10:45:54 +0200

    fix

--- apps/blowfish_varRes/runArguments00.sh
+++ apps/blowfish_varRes/runArguments00.sh
@@ -1,1 +1,1 @@
-../launchSim.sh -bFreeSpace 0 -muteAll 1 -bpdx 4 -bpdy 4 -tdump 0.5 -nu 0.0004 -tend 0 -shapes 'blowfish L=0.16 bFixed=1'
+../launchSim.sh -bFreeSpace 0 -muteAll 1 -bpdx 4 -bpdy 4 -tdump 0.0 -nu 0.0004 -tend 0 -shapes 'blowfish radius=0.16 bFixed=1'
