b4bf0c99339f34f3540f1d291232585282c23000
novatig <novatig@ethz.ch>
2018-08-20 20:27:34 +0200

    minor

--- apps/cylFollow_varRes/setup.sh
+++ apps/cylFollow_varRes/setup.sh
@@ -7,5 +7,5 @@ cat <<EOF >${BASEPATH}${RUNFOLDER}/appSettings.sh
 SETTINGS+=" --appSettings runArguments00.sh;runArguments01.sh;runArguments02.sh;runArguments03.sh "
 SETTINGS+=" --nStepPappSett 4194304;2097152;1048576;0 "
 EOF
-chmod +x ${BASEPATH}${RUNFOLDER}/appSettings.sh
+chmod 755 ${BASEPATH}${RUNFOLDER}/appSettings.sh
 
