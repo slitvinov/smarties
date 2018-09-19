11e2784e4b90d6e9beed9d6dba73484e2d7cf7c6
Sid Verma <sverma@ethz.ch>
2018-09-19 20:40:28 +0200

    code ist gebroken, fix it...

--- apps/boatNav/boatNav.cpp
+++ apps/boatNav/boatNav.cpp
@@ -19,9 +19,10 @@ using namespace std;
 
 struct Entity
 {
-  //const unsigned nQuadrants; // NOTE: not used at the moment. Should we just stick to angles??
   const unsigned nStates;
-  const double maxSpeed;
+  const double length;
+  //const double maxSpeed, length;
+  const double startX[2], endX[2];
   std::mt19937& genA;
   bool isOver=false;
 
@@ -32,15 +33,15 @@ struct Entity
   double speed; 
 
   void reset() {
-    uniform_real_distribution<double> distrib(0, EXTENT);
-    p[0] = distrib(genA);
-    p[1] = distrib(genA);
+    normal_distribution<double> distrib(0, 2*length);
+    p[0] = this->startX[0] + distrib(genA);
+    p[1] = this->startX[1] + distrib(genA);
     speed = maxSpeed;
 	isOver = false;
 	}
 
   bool is_over() {
-    return isOver; // TODO add catching condition - EHH??
+    return isOver; 
   }
 
   void advance(vector<double> act) {
@@ -77,35 +78,28 @@ struct Entity
 };
 
 
-struct Prey: public Entity
+struct SUV: public Entity
 {
-  const double stdNoise; // Only prey assumed to suffer from noise
+  const double stdNoise; // Only nemo assumed to suffer from noise
 
-  Prey(std::mt19937& _gen, const unsigned _nStates, const double vM, const double dN)
+  SUV(std::mt19937& _gen, const unsigned _nStates, const double vM, const double dN)
     : Entity(_gen, _nStates, vM), stdNoise(dN) {}
 
   template<typename T>
   vector<double> getState(const T& E1, const T& E2) { // wrt enemy E
     vector<double> state(nStates, 0);
-    state[0] = p[0];
-    state[1] = p[1];
+    state[0] = p[0]; // dx
+    state[1] = p[1]; // dy
     const double angEnemy1 = getAngle(E1);
-    const double angEnemy2 = getAngle(E2);
-    const double distEnemy1 = getDistance(E1);
-    const double distEnemy2 = getDistance(E2);
     const double distEnemy = distEnemy1*distEnemy2;
-    // close? low noise. moving slow? low noise
-    //const double noiseAmp = stdNoise*distEnemy*speed/std::pow(maxSpeed,2);
-    // or, can also think of it as ETA (Estimated Time of Arrival)
-    const double ETA = distEnemy/fabs(speed);
+
+	const double ETA = distEnemy/fabs(speed);
     const double noiseAmp = stdNoise*ETA;
     std::normal_distribution<double> distrib(0, noiseAmp); // mean=0, stdDev=noiseAmp
-    const double noisyAng1 = angEnemy1 + distrib(genA);
-    const double noisyAng2 = angEnemy2 + distrib(genA);
+
+	const double noisyDy = angEnemy1 + distrib(genA);
     state[2] = distEnemy1*std::cos(noisyAng1); 
     state[3] = distEnemy1*std::sin(noisyAng1);
-    state[4] = distEnemy2*std::cos(noisyAng2);
-    state[5] = distEnemy2*std::sin(noisyAng2);
     return state;
   }
 
@@ -129,34 +123,6 @@ struct Prey: public Entity
  
 };
 
-struct Predator: public Entity
-{
-  const double velPenalty;
-  Predator(std::mt19937& _gen, const unsigned _nStates, const double vM, const double vP)
-    : Entity(_gen, _nStates, vP*vM), velPenalty(vP) {}
-
-  template<typename T1, typename T2>
-  vector<double> getState(const T1& _prey, const T2& _pred) const { // wrt enemy (or adversary) E
-    vector<double> state(nStates, 0);
-    state[0] = p[0];
-    state[1] = p[1];
-    const double angPrey = getAngle(_prey); // No noisy angle for predator
-    const double angPred = getAngle(_pred); // No noisy angle for predator
-    const double distPrey = getDistance(_prey);
-    const double distPred = getDistance(_pred);
-    state[2] = distPrey*std::cos(angPrey);
-    state[3] = distPrey*std::sin(angPrey);
-    state[4] = distPred*std::cos(angPred);
-    state[5] = distPred*std::sin(angPred);
-    return state;
-  }
-
-  template<typename T1, typename T2>
-  double getReward(const T1& _prey, const T2& _pred) const {
-    const double distMult = _prey.getDistance(*this) * _prey.getDistance(_pred);
-    return - distMult; // cooperative predators
-  }
-};
 
 int main(int argc, const char * argv[])
 {
@@ -165,64 +131,44 @@ int main(int argc, const char * argv[])
   const unsigned maxStep = 500;
   const int control_vars = 2; // 2 components of velocity
   const int state_vars = 6;   // number of states (self pos[2], enemy cos|sin[theta])
-  const int number_of_agents = 3; // 2 predator, 1 prey
+  const int number_of_agents = 3; // 2 predator, 1 nemo
   //Sim box has size EXTENT. Fraction of box that agent can traverse in 1 step:
   const double maxSpeed = 0.02 * EXTENT/dt;
   //socket number is given by RL as first argument of execution
   Communicator comm(socket, state_vars, control_vars, number_of_agents);
 
-  // predator last arg is how much slower than prey (eg 50%)
-  Predator pred1(comm.gen, state_vars, maxSpeed, 0.5);
-  Predator pred2(comm.gen, state_vars, maxSpeed, 0.5);
-  // prey last arg is observation noise (eg ping of predator is in 1 stdev of noise)
-  // Prey     prey(comm.gen, state_vars, maxSpeed, 1.0); // The noise was large, the prey didn't run away quickly if preds were far away
-  Prey     prey(comm.gen, state_vars, maxSpeed, 0.0);
+  SUV     nemo(comm.gen, state_vars, maxSpeed, 0.0);
 
   unsigned sim = 0;
   while(true) //train loop
   {
     //reset environment:
-    pred1.reset();
-    pred2.reset();
-    prey.reset();
+    nemo.reset();
 
     //send initial state
-    comm.sendInitState(pred1.getState(prey,pred2), 0);
-    comm.sendInitState(pred2.getState(prey,pred1), 1);
-    comm.sendInitState(prey.getState(pred1,pred2), 2);
+    comm.sendInitState(pred1.getState(nemo,pred2), 0);
 
     unsigned step = 0;
     while (true) //simulation loop
     {
-      pred1.advance(comm.recvAction(0));
-      pred2.advance(comm.recvAction(1));
-      prey.advance(comm.recvAction(2));
+      nemo.advance(comm.recvAction(0));
 
-	  vector<bool> gotCaught = prey.checkTermination(pred1,pred2);
-	  if(prey.is_over()){ // Terminate simulation 
-		  // Cooperative hunting - both predators get reward
+	  vector<bool> gotCaught = nemo.checkTermination(pred1,pred2);
+	  if(nemo.is_over()){ // Terminate simulation 
 		  const double finalReward = 10*EXTENT;
+		  comm.sendTermState(nemo.getState(pred1,pred2),-finalReward, 2);
 
-		  // Competitive hunting - only one winner, other one gets jack (also, change the reward to be not d1*d2, but just d_i if use competitive)
-          comm.sendTermState(pred1.getState(prey,pred2), finalReward*gotCaught[0], 0);
-          comm.sendTermState(pred2.getState(prey,pred1), finalReward*gotCaught[1], 1);
-		  comm.sendTermState(prey.getState(pred1,pred2),-finalReward, 2);
-
-		  printf("Sim #%d reporting that prey got its world rocked.\n", sim); fflush(NULL);
+		  printf("Sim #%d reporting that nemo got its world rocked.\n", sim); fflush(NULL);
 		  sim++; break;
 	  }
 
       if(step++ < maxStep)
       {
-        comm.sendState(  pred1.getState(prey,pred2), pred1.getReward(prey,pred2), 0);
-        comm.sendState(  pred2.getState(prey,pred1), pred2.getReward(prey,pred1), 1);
-        comm.sendState(  prey.getState(pred1,pred2), prey.getReward(pred1,pred2), 2);
+        comm.sendState(  nemo.getState(pred1,pred2), nemo.getReward(pred1,pred2), 2);
       }
       else
       {
-        comm.truncateSeq(pred1.getState(prey,pred2), pred1.getReward(prey,pred2), 0);
-        comm.truncateSeq(pred2.getState(prey,pred1), pred2.getReward(prey,pred2), 1);
-        comm.truncateSeq(prey.getState(pred1,pred2), prey.getReward(pred1,pred2), 2);
+        comm.truncateSeq(nemo.getState(pred1,pred2), nemo.getReward(pred1,pred2), 2);
         sim++;
         break;
       }
