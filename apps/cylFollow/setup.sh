7436990543801643bfdb147a01c56e876485a6b4
novatig <novatig@ethz.ch>
2018-09-27 09:24:37 +0200

    cyl fish settings update

--- apps/cylFollow/setup.sh
+++ apps/cylFollow/setup.sh
@@ -6,8 +6,9 @@ make -C ../makefiles/ app=cylFollow -j4 #config=debug
 #config=segf
 cat <<EOF >${BASEPATH}${RUNFOLDER}/appSettings.sh
 SETTINGS+=" --appSettings runArguments00.sh,runArguments01.sh,runArguments02.sh,runArguments03.sh "
-SETTINGS+=" --nStepPappSett 2097152,1048576,524288,0 "
+SETTINGS+=" --nStepPappSett 3531072,524288,131072,0 "
 EOF
 chmod 755 ${BASEPATH}${RUNFOLDER}/appSettings.sh
 
+#SETTINGS+=" --nStepPappSett 2097152,1048576,524288,0 "
 #SETTINGS+=" --nStepPappSett 4194304,2097152,1048576,0 "
