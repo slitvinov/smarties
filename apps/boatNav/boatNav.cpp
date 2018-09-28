85267b6f7dd7de72e0325b905e1e555abb6c4aa0
Sid Verma <vermas@fau.edu>
2018-09-28 18:10:04 -0400

    boatNav should compile now

--- apps/boatNav/boatNav.cpp
+++ apps/boatNav/boatNav.cpp
@@ -11,7 +11,7 @@
 #define dt 1.0e-4
 #define SAVEFREQ 1
 #define STEPFREQ 1
-#define maxStep = 500000
+#define maxStep 500000
 
 using namespace std;
 
@@ -27,9 +27,6 @@ struct Entity
 	bool isOver=false;
 
 	vector<double> u,v,r, uDot, vDot, rDot, x,y,thetaR;
-	u.reserve(maxStep), v.reserve(maxStep), r.reserve(maxStep);
-	uDot.reserve(maxStep), vDot.reserve(maxStep), rDot.reserve(maxStep);
-	x.reserve(maxStep), y.reserve(maxStep), thetaR.reserve(maxStep);
 
 	Entity(std::mt19937& _gen, const unsigned nQ, const double xS[2], const double xE[2]) : nStates(nQ), genA(_gen)
 	{
@@ -38,6 +35,10 @@ struct Entity
 		pathEnd[0] = xE[0]; 
 		pathEnd[1] = xE[1];
 		thetaPath = atan2( (pathEnd[1] - pathStart[1]), (pathEnd[0] - pathStart[0]) ); 
+
+		u.reserve(maxStep); v.reserve(maxStep); r.reserve(maxStep);
+		uDot.reserve(maxStep); vDot.reserve(maxStep); rDot.reserve(maxStep);
+		x.reserve(maxStep); y.reserve(maxStep); thetaR.reserve(maxStep);
 	}
 
 	array<double, 2> p; // absolute position
@@ -95,9 +96,9 @@ struct Entity
 		return std::atan2(relY, relX) - thetaPath;
 	}
 
-	double getDistance() const {
-		double relX = E.p[0] - p[0];
-		double relY = E.p[1] - p[1];
+	double getDistance(const double xLoc[2]) const {
+		double relX = p[0] - xLoc[0];
+		double relY = p[1] - xLoc[1];
 		return std::sqrt(relX*relX + relY*relY);
 	}
 };
@@ -107,37 +108,38 @@ struct USV: public Entity
 {
   const double stdNoise; // Only boat assumed to suffer from noise
 
-  USV(std::mt19937& _gen, const unsigned _nStates, const double vM, const double dN)
-    : Entity(_gen, _nStates, vM), stdNoise(dN) {}
+  USV(std::mt19937& _gen, const unsigned _nStates, const double dN, const double xPathStart[2], const double xPathEnd[2])
+    : Entity(_gen, _nStates, xPathStart, xPathEnd), stdNoise(dN) {}
 
   // State vector contains : 
-  template<typename T>
-  vector<double> getState() { // wrt path
+  vector<double> getState() const { // wrt path
 
 	  vector<double> state(nStates, 0);
 
-	  state[0] = p[0]; // dx
-	  state[1] = p[1]; // dy
-	  const double angEnemy1 = getAngle(E1);
-	  const double distEnemy = distEnemy1*distEnemy2;
+	  state[0] = getDistance(pathStart); 	// rPos
+	  state[1] = getAngle(); 		// thetaPos
+	  state[2] = thetaR.back(); 		// bearing nose
+	  state[3] = u.back();
+	  state[4] = v.back();
+	  state[5] = r.back();
 
-	  const double ETA = distEnemy/fabs(speed);
-	  const double noiseAmp = stdNoise*ETA;
-	  std::normal_distribution<double> distrib(0, noiseAmp); // mean=0, stdDev=noiseAmp
-
-	  const double noisyDy = angEnemy1 + distrib(genA);
-	  state[2] = distEnemy1*std::cos(noisyAng1); 
+	  //const double noiseAmp = stdNoise*ETA;
+	  //std::normal_distribution<double> distrib(0, noiseAmp); // mean=0, stdDev=noiseAmp
+	  //const double noisyDy = angEnemy1 + distrib(genA);
+	  //state[2] = distEnemy1*std::cos(noisyAng1); 
 	  return state;
   }
 
-  double getReward(const ) const {
-	  return -/params.l;
+  double getReward() const {
+	  // Compute perpendicular distance from path
+	  const double perpDist = getDistance(pathStart)*cos(getAngle());
+	  return -perpDist/params.l; // normalize with ship width
   }
 
-  vector<bool> checkTermination() {
-	  const double threshold= 0.1*this->width;
-	  const double distGoal = getDistance(E1);
-	  const bool goal = (distCheckpoint < threshold) ? true : false;
+  bool checkTermination() {
+	  const double threshold= 0.1*this->params.l; // within 10% of ship width
+	  const double distGoal = getDistance(pathEnd);
+	  const bool goal = (distGoal < threshold) ? true : false;
 	  if (goal) isOver = true; 
 	  return goal;
   }
@@ -148,16 +150,21 @@ struct USV: public Entity
 int main(int argc, const char * argv[])
 {
 
-  //const unsigned maxStep = 500000;
   const int control_vars = 2; // 2 components of thrust
-  const int state_vars = 6;   // number of states (self pos[2], )
+  const int state_vars = 6;   // number of states
   const int number_of_agents = 1;
 
   //communication:
   const int socket = std::stoi(argv[1]);
   //socket number is given by RL as first argument of execution
-  Communicator 	comm(socket, state_vars, control_vars, number_of_agents);
-  SUV 		boat(comm.gen, state_vars, 0.0);
+  Communicator	comm(socket, state_vars, control_vars, number_of_agents);
+  std::mt19937	&rngPointer =  comm.getPRNG();
+
+  // Path start and end
+  const double xPathStart[2] = {0,0};
+  const double xPathEnd[2] = {10,0};
+
+  USV		boat(rngPointer, state_vars, 0.0, xPathStart, xPathEnd);
 
   unsigned sim = 0;
   while(true) //train loop
@@ -171,9 +178,9 @@ int main(int argc, const char * argv[])
     unsigned step = 0;
     while (true) //simulation loop
     {
-      boat.advance(comm.recvAction(0), step);
+      boat.advance(comm.recvAction(0));
 
-	  vector<bool> reachedGoal = boat.checkTermination();
+	  const bool reachedGoal = boat.checkTermination();
 	  if(boat.is_over()){ // Terminate simulation 
 		  const double finalReward = 10*boat.params.l;
 		  comm.sendTermState(boat.getState(),finalReward, 0);
