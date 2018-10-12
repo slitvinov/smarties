6265a0a30141335fbe8b43559367ee940b0bcc4d
Sid Verma <sverma@ethz.ch>
2018-10-13 01:11:31 +0200

    doh

--- launch/create_rundir.sh
+++ launch/create_rundir.sh
@@ -20,5 +20,5 @@ if [ ${HOST:0:5} == 'euler' ] || [ ${HOST:0:3} == 'eu-' ] ; then
 	export BASEPATH="/cluster/scratch/${MYNAME}/smarties/"
 else
 export BASEPATH="../runs/"
-#fi
+fi
 mkdir -p ${BASEPATH}${RUNFOLDER}
