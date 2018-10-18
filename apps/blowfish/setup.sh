a21560165c97d12ffc9a0a911ce14ffd096c1590
Guido Novati <novatig@ethz.ch>
2018-10-18 14:34:46 +0200

    minor setup

--- apps/blowfish/setup.sh
+++ apps/blowfish/setup.sh
@@ -5,7 +5,7 @@ make -C ../makefiles/  app=blowfish -j4
 
 cat <<EOF >${BASEPATH}${RUNFOLDER}/appSettings.sh
 SETTINGS+=" --appSettings runArguments00.sh,runArguments01.sh,runArguments02.sh,runArguments03.sh "
-SETTINGS+=" --nStepPappSett 2097152,1048576,524288,0 "
+SETTINGS+=" --nStepPappSett 262144,262144,262144,0 "
 EOF
 chmod 755 ${BASEPATH}${RUNFOLDER}/appSettings.sh
 
--- apps/cylFollow/setup.sh
+++ apps/cylFollow/setup.sh
@@ -6,7 +6,7 @@ make -C ../makefiles/ app=cylFollow -j4 #config=debug
 #config=segf
 cat <<EOF >${BASEPATH}${RUNFOLDER}/appSettings.sh
 SETTINGS+=" --appSettings runArguments00.sh,runArguments01.sh,runArguments02.sh,runArguments03.sh "
-SETTINGS+=" --nStepPappSett 3531072,524288,131072,0 "
+SETTINGS+=" --nStepPappSett 524288,262144,131072,0 "
 EOF
 chmod 755 ${BASEPATH}${RUNFOLDER}/appSettings.sh
 
