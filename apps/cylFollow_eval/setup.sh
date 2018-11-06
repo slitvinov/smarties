4b1a993435f8a2e9035b20d97a3c3a61f34a6ee5
pvlachas <pvlachas@ethz.ch>
2018-11-06 20:55:34 +0100

    cylFollow_eval corrected make.flags and setup.sh

--- apps/cylFollow_eval/setup.sh
+++ apps/cylFollow_eval/setup.sh
@@ -1,8 +1,8 @@
 cp ../apps/cylFollow_eval/runArguments* ${BASEPATH}${RUNFOLDER}/
-cp ../apps/blowfish_eval/agent_00* ${BASEPATH}${RUNFOLDER}/
+cp ../apps/cylFollow_eval/agent_00* ${BASEPATH}${RUNFOLDER}/
 
-make -C ../makefiles/ clean
-make -C ../makefiles/ app=cylFollow_eval precision=single -j4 #config=segf
+#make -C ../makefiles/ clean
+#make -C ../makefiles/ app=cylFollow_eval precision=single -j4 #config=segf
 
 cat <<EOF >${BASEPATH}${RUNFOLDER}/appSettings.sh
 SETTINGS+=" --appSettings runArguments00.sh "
--- launch/settings/settings_VRACER_expensiveData_eval.sh
+++ launch/settings/settings_VRACER_expensiveData_eval.sh
@@ -22,7 +22,7 @@ SETTINGS+=" --outWeightsPrefac 0.1"
 #whether you are training a policy or testing an already trained network
 SETTINGS+=" --bTrain 0"
 #RL algorithm: NAF, DPG are continuous actions, NFQ (also accepted DQN) is for discrete actions
-SETTINGS+=" --learner RACER"
+SETTINGS+=" --learner VRACER"
 #Initialization of the standard deviation for all actions
 SETTINGS+=" --explNoise 0.0"
 #Number of time steps per gradient step
