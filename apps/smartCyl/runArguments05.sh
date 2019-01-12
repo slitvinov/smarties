4bd6701324fe6a8fcfc328d94a6195c65771f1fb
Guido Novati <novatig@ethz.ch>
2019-01-12 22:17:32 +0100

    minor

---
+++ apps/smartCyl/runArguments05.sh
@@ -0,0 +1,1 @@
+../launchSim.sh -bFreeSpace 0 -bNeumann 1 -muteAll 1 -bpdx  8 -bpdy  4 -tdump 0.0 -nu .00004 -tend 0 -shapes 'halfDisk_radius=.05_xpos=.2_bForced=1_bFixed=1_xvel=.1_tAccel=5,smartDisk_radius=.05_xpos=.5_bFixedy=1_bForced=0'
