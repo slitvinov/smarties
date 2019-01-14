6479511006f8658ca575125da5df9d518cc6db4f
Guido Novati <novatig@ethz.ch>
2019-01-14 17:29:24 +0100

    minor

--- apps/smartCyl/runArguments05.sh
+++ apps/smartCyl/runArguments05.sh
@@ -1,1 +1,1 @@
-../launchSim.sh -bFreeSpace 0 -bNeumann 1 -muteAll 1 -bpdx  8 -bpdy  4 -tdump 0.0 -nu .00004 -tend 0 -shapes 'halfDisk_radius=.05_xpos=.2_bForced=1_bFixed=1_xvel=.1_tAccel=5,smartDisk_radius=.05_xpos=.5_bFixedy=1_bForced=0'
+../launchSim.sh -bFreeSpace 0 -bNeumann 1 -muteAll 1 -bpdx 16 -bpdy  8 -tdump 0.0 -nu .00004 -tend 0 -shapes 'halfDisk_radius=.05_xpos=.2_bForced=1_bFixed=1_xvel=.1,smartDisk_radius=.05_xpos=.5_bFixedy=1_xvel=.1'
--- apps/smartCyl/setup.sh
+++ apps/smartCyl/setup.sh
@@ -6,7 +6,7 @@ rm ../makefiles/rl
 make -C ../makefiles/ app=smartCyl precision=single -j4
 #config=segf
 cat <<EOF >${BASEPATH}${RUNFOLDER}/appSettings.sh
-SETTINGS+=" --appSettings runArguments05.sh,runArguments00.sh,runArguments01.sh"
-SETTINGS+=" --nStepPappSett 2097152,2097152,0 "
+SETTINGS+=" --appSettings runArguments00.sh"
+SETTINGS+=" --nStepPappSett 0 "
 EOF
 chmod 755 ${BASEPATH}${RUNFOLDER}/appSettings.sh
