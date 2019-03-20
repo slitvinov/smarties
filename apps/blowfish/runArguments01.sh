68139f3f8ef4d32e7e00cf7c6a933932bbd5a1cf
Guido Novati <novatig@ethz.ch>
2019-03-20 13:06:19 +0100

    new naming convention for CUP2D BC

--- apps/blowfish/runArguments01.sh
+++ apps/blowfish/runArguments01.sh
@@ -1,1 +1,1 @@
-../launchSim.sh -bFreeSpace 0 -bNeumann 1 -muteAll 1 -bpdx 16 -bpdy 16 -tdump 0.0 -nu 0.0004 -tend 0 -shapes 'blowfish radius=0.16 bFixed=1'
+../launchSim.sh -poissonType cosine -muteAll 1 -bpdx 16 -bpdy 16 -tdump 0.0 -nu 0.0004 -tend 0 -shapes 'blowfish radius=0.16 bFixed=1'
