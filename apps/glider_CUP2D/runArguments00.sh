45ad4a34951f8821350464e6c6f1e67d937e315f
Guido Novati <novatig@ethz.ch>
2019-03-21 22:31:02 +0100

    minor, put the settings file in the setup script

--- apps/glider_CUP2D/runArguments00.sh
+++
@@ -1,1 +0,0 @@
-../launchSim.sh -poissonType cosine -muteAll 1 -bpdx 32 -bpdy 32 -tdump 1 -nu 0.000025 -tend 0 -shapes 'glider_semiAxisX=.125_semiAxisY=.025_rhoS=1.01_xpos=.5_ypos=.5_bFixed=1_bForced=0'
--- apps/glider_CUP2D/setup.sh
+++ apps/glider_CUP2D/setup.sh
@@ -1,9 +1,18 @@
-cp ../apps/glider_CUP2D/runArguments* ${BASEPATH}${RUNFOLDER}/
+#cp ../apps/glider_CUP2D/runArguments* ${BASEPATH}${RUNFOLDER}/
 
 if [[ "${SKIPMAKE}" != "true" ]] ; then
 make -C ../makefiles/ clean
 make -C ../makefiles/ app=glider_CUP2D  -j4
 fi
+
+# notes: eta_y is a param of mesh_density, implement noisy init cond
+#cat <<EOF >${BASEPATH}${RUNFOLDER}/runArguments00.sh
+#../launchSim.sh -poissonType cosine -muteAll 1 -bpdx 32 -bpdy 32 -tdump 1 -nu 0.000025 -tend 0 -shapes 'glider_semiAxisX=.125_semiAxisY=.025_rhoS=1.01_xpos=.5_ypos=.5_bFixed=1_bForced=0'
+#EOF
+
+cat <<EOF >${BASEPATH}${RUNFOLDER}/runArguments00.sh
+../launchSim.sh -poissonType cosine -muteAll 1 -bpdx 32 -bpdy 32 -tdump 1 -nu 0.0002 -tend 0 -shapes 'glider_semiAxisX=.125_semiAxisY=.025_rhoS=2_xpos=.6_ypos=.4_bFixed=1_bForced=0'
+EOF
 
 cat <<EOF >${BASEPATH}${RUNFOLDER}/appSettings.sh
 SETTINGS+=" --appSettings runArguments00.sh"
