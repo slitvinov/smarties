73f13947baa571a8f618edf3a68d28d0a68b19e9
webepasc <webepasc@student.ethz.ch>
2019-03-27 14:44:58 +0100

    changed configuration file and makefile for leadfollow

--- apps/leadFollow/runArguments00.sh
+++ apps/leadFollow/runArguments00.sh
@@ -1,1 +1,2 @@
-../launchSim.sh -bFreeSpace 0 -muteAll 1 -bpdx 16 -bpdy 8 -tdump 0.0 -nu 0.00001 -tend 0 -shapes 'stefanfish L=0.2 xpos=0.15 bFixed=1 pid=1,stefanfish L=0.2 xpos=0.45 bFixedy=1'
+# CAREFULL, the first fish has to be placed at xpos=L/2, the second is assumed to have fixed length and follows at xpos=3L/2
+../launchSim.sh -bFreeSpace 0 -muteAll 0 -bpdx 32 -bpdy 16 -tdump 0.1 -nu 0.00001 -tend 0 -shapes 'stefanfish L=0.4 xpos=0.2 bFixed=1 pid=1,stefanfish L=0.2 xpos=0.6 bFixedy=1'
--- apps/leadFollow/setup.sh
+++ apps/leadFollow/setup.sh
@@ -2,11 +2,11 @@ cp ../apps/leadFollow/runArguments* ${BASEPATH}${RUNFOLDER}/
 
 if [[ "${SKIPMAKE}" != "true" ]] ; then
 make -C ../makefiles/ clean
-make -C ../makefiles/ app=leadFollow -j4 #config=segf
+make -C ../makefiles/ app=leadFollow -j #config=segf
 fi
 
 cat <<EOF >${BASEPATH}${RUNFOLDER}/appSettings.sh
-SETTINGS+=" --appSettings runArguments00.sh,runArguments01.sh,runArguments02.sh,runArguments03.sh "
-SETTINGS+=" --nStepPappSett 4194304,2097152,1048576,0 "
+SETTINGS+=" --appSettings runArguments00.sh "
+SETTINGS+=" --nStepPappSett 4194304 "
 EOF
 chmod 755 ${BASEPATH}${RUNFOLDER}/appSettings.sh
