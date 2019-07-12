c163b373df221a4b73d7ad11be104e806a9ec8e3
Guido Novati <novatig@ethz.ch>
2019-07-12 19:16:33 +0300

    minor

--- source/Network/Builder.cpp
+++ source/Network/Builder.cpp
@@ -190,28 +190,28 @@ void Builder::addConv2d(const Conv2D_Descriptor& descr, bool bOut, Uint iLink)
   // is: outSize should be : (InSize - FilterSize + 2*Padding)/Stride + 1
   if (      matchConv2D(descr, 84,84, 4, 8,8,32, 4,4, 0,0, 20,20) ) {
     layers.emplace_back(
-      std::make_unique<Mat2ImLayer<       84,84, 4, 8,8,32, 4,4, 0,0, 20,20> >
+      std::make_unique<Mat2ImLayer<         84,84, 4, 8,8,32, 4,4, 0,0, 20,20>>
         (ID, false, iLink) );
     layers.emplace_back(
-      std::make_unique<Conv2DLayer<LRelu, 84,84, 4, 8,8,32, 4,4, 0,0, 20,20> >
+      std::make_unique<Conv2DLayer<SoftSign,84,84, 4, 8,8,32, 4,4, 0,0, 20,20>>
         (ID+1, bOut, 1) );
   }
   else
   if (      matchConv2D(descr, 20,20,32, 4,4,64, 2,2, 0,0,  9, 9) ) {
     layers.emplace_back(
-      std::make_unique<Mat2ImLayer<       20,20,32, 4,4,64, 2,2, 0,0,  9, 9> >
+      std::make_unique<Mat2ImLayer<         20,20,32, 4,4,64, 2,2, 0,0,  9, 9>>
         (ID, false, iLink) );
     layers.emplace_back(
-      std::make_unique<Conv2DLayer<LRelu, 20,20,32, 4,4,64, 2,2, 0,0,  9, 9> >
+      std::make_unique<Conv2DLayer<SoftSign,20,20,32, 4,4,64, 2,2, 0,0,  9, 9>>
         (ID+1, bOut, 1) );
   }
   else
   if (      matchConv2D(descr,  9, 9,64, 3,3,64, 1,1, 0,0,  7, 7) ) {
     layers.emplace_back(
-      std::make_unique<Mat2ImLayer<        9, 9,64, 3,3,64, 1,1, 0,0,  7, 7> >
+      std::make_unique<Mat2ImLayer<          9, 9,64, 3,3,64, 1,1, 0,0,  7, 7>>
         (ID, false, iLink) );
     layers.emplace_back(
-      std::make_unique<Conv2DLayer<LRelu,  9, 9,64, 3,3,64, 1,1, 0,0,  7, 7> >
+      std::make_unique<Conv2DLayer<SoftSign, 9, 9,64, 3,3,64, 1,1, 0,0,  7, 7>>
         (ID+1, bOut, 1) );
   }
   else
