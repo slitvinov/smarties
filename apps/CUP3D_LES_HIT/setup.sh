df697abb86098aef651456ed8df4c6e658b4b595
Guido Novati <novatig@gmail.com>
2019-09-17 22:24:05 +0200

    2 bugfixes: in Agent and in memory processing. Some renaming and new residual layer

--- apps/CUP3D_LES_HIT/setup.sh
+++ apps/CUP3D_LES_HIT/setup.sh
@@ -10,28 +10,18 @@ fi
 cp ${COMPILEDIR}/rlHIT ${RUNDIR}/exec
 
 # write simulation settings files:
-NNODEX=${NNODEX:-1}
-NNODEY=${NNODEY:-1}
-NNODEZ=${NNODEZ:-1}
-NNODES=$(($NNODEX * $NNODEY * $NNODEZ))
-BPDX=${BPDX:-4}
-BPDY=${BPDY:-${BPDX}} #${BPDY:-32}
-BPDZ=${BPDZ:-${BPDX}} #${BPDZ:-32}
-NU=${NU:-0.005}
-
 cat <<EOF >${RUNDIR}/runArguments00.sh
-./simulation -bpdx ${BPDX} -bpdy ${BPDY} -bpdz ${BPDZ} -extentx 6.2831 \
+./simulation -bpdx 4 -bpdy 4 -bpdz 4 -extentx 6.2831 -tend 500 -CFL 0.1 \
 -dump2D 0 -dump3D 0 -tdump 0.0 -BC_x periodic -BC_y periodic -BC_z periodic \
--initCond HITurbulence -spectralIC fromFile -keepMomentumConstant 1 \
--spectralICFile ../meanTarget.dat -nprocsx ${NNODEX} -nprocsy ${NNODEY} \
--nprocsz ${NNODEZ} -CFL 0.1 -tend 50 -sgs RLSM -compute-dissipation 1 \
--spectralForcing 1 -analysis HIT -tAnalysis 0.1 -nu ${NU}
+-initCond HITurbulence -spectralIC fromFit -keepMomentumConstant 1 -nu 0.005 \
+-nprocsx 1 -nprocsy 1 -nprocsz 1 -sgs RLSM -compute-dissipation 1  -cs 0.25 \
+-spectralForcing 1 -analysis HIT -tAnalysis 0.1 -energyInjectionRate 0.2
 EOF
 
 #copy target files
-cp targetHIT/re75/meanTarget.dat  ${RUNDIR}/
-cp targetHIT/re75/kdeTarget.dat   ${RUNDIR}/
-cp targetHIT/re75/scaleTarget.dat ${RUNDIR}/
+#cp targetHIT/re75/meanTarget.dat  ${RUNDIR}/
+#cp targetHIT/re75/kdeTarget.dat   ${RUNDIR}/
+#cp targetHIT/re75/scaleTarget.dat ${RUNDIR}/
 
-export MPI_RANKS_PER_ENV=$NNODES
+export MPI_RANKS_PER_ENV=1
 export EXTRA_LINE_ARGS=" --appSettings runArguments00.sh --nStepPappSett 0 "
