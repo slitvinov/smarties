a079262e6808a7187db70d51f6939435a5427a96
Pascal Weber <webepasc@student.ethz.ch>
2019-12-09 16:35:21 +0100

    modifies runargs for cylfollow

--- apps/CUP2D_cylFollow/runArguments01.sh
+++ apps/CUP2D_cylFollow/runArguments01.sh
@@ -1,1 +1,1 @@
-../launchSim.sh -poissonType cosine -muteAll 1 -bpdx 32 -bpdy 16 -tdump 0 -nu 0.000015625 -tend 0 -shapes 'halfDisk_radius=.03125_xpos=.1_bForced=1_bFixed=1_xvel=.125_tAccel=5,stefanfish_L=.2_xpos=.4_bFixedy=1'
+../launchSim.sh -poissonType cosine -muteAll 1 -bpdx 32 -bpdy 16 -tdump 0 -nu 0.000018 -tend 0 -shapes 'halfDisk_radius=.06_angle=20_xpos=.2_bForced=1_bFixed=1_xvel=0.15_tAccel=5,stefanfish_L=.2_xpos=.5'
