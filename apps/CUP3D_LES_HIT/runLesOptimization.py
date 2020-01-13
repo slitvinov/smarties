c2d71e03ed82f9b33ddf6c563d0814e4b3134a6a
Guido Novati <novatig@ethz.ch>
2020-01-13 17:10:47 +0100

    updated the search-for-css script

--- apps/CUP3D_LES_HIT/runLesOptimization.py
+++ apps/CUP3D_LES_HIT/runLesOptimization.py
@@ -4,44 +4,53 @@ import re, argparse, numpy as np, glob, subprocess
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
+def    tkeFit(nu, eps): return 2.86302040 * np.power(eps, 2/3.0)
+#def    relFit(nu, eps): return 7.33972668 * np.power(eps, 1/6.0) / np.sqrt(nu)
+def    relFit(nu, eps):
+    tke = tkeFit(nu,eps)
+    uprime = np.sqrt(2.0/3.0 * tke);
+    lambd = np.sqrt(15 * nu / eps) * uprime;
+    return uprime * lambd / nu;
 
-def findAllParams(path):
-    NUs, EPSs = set(), set()
-    alldirs = glob.glob(path+'/scalars_*')
-    for dirn in alldirs:
-        EPSs.add(re.findall('EPS\d.\d\d\d',  dirn)[0][3:])
-        NUs. add(re.findall('NU\d.\d\d\d\d', dirn)[0][2:])
-    NUs, EPSs  = list( NUs), list(EPSs)
-    NUs.sort()
-    EPSs.sort()
-    for i in range(len(NUs)): NUs[i] = float(NUs[i])
-    for i in range(len(EPSs)): EPSs[i] = float(EPSs[i])
-    return NUs, EPSs
+def epsNuFromRe(Re, uEta = 1.0):
+    C = 2.87657077
+    K = 2/3.0 * C * np.sqrt(15)
+    eps = np.power(uEta*uEta * Re / K, 3.0/2.0)
+    nu = np.power(uEta, 4) / eps
+    return eps, nu
 
-def runspec(nu, eps, cs, run):
-    return "HIT_BOX2_LES_EXT2pi_EPS%.03f_NU%.04f_CS%.03f_RUN%d" \
-               % (eps, nu, cs, run)
+def runspec(re, cs, run):
+    return "HIT_BOX2_LES_EXT2pi_RE%03d_CS%.03f_RUN%d" % (re, cs, run)
 
 def getSettings(nu, eps, cs):
     options = '-sgs SSM -cs %f -bpdx 4 -bpdy 4 -bpdz 4 -CFL 0.1 ' % cs
     tAnalysis = np.sqrt(nu / eps)
-    return options + '-extentx 6.2831853072 -dump2D 0 -dump3D 0 ' \
-       '-tdump 1 -BC_x periodic -BC_y periodic -BC_z periodic ' \
-       '-spectralIC fromFit -initCond HITurbulence -tAnalysis %f ' \
+    tEnd = 1000 * tAnalysis
+    return options + '-extentx 6.2831 -dump2D 0 -dump3D 0 ' \
+       '-tdump 0 -BC_x periodic -BC_y periodic -BC_z periodic ' \
+       '-spectralIC fromFile -initCond HITurbulence -tAnalysis %f ' \
        '-compute-dissipation 1 -nprocsx 1 -nprocsy 1 -nprocsz 1 ' \
-       '-spectralForcing 1 -tend 200  -keepMomentumConstant 1 ' \
+       '-spectralForcing 1 -tend %f -keepMomentumConstant 1 ' \
        '-analysis HIT -nu %f -energyInjectionRate %f ' \
-       % (tAnalysis, nu, eps)
+       % (tAnalysis, tEnd, nu, eps)
 
-def launchEuler(nu, eps, cs, run):
-    cmd = "FOLDER=~/CubismUP_3D/runs/%s\n " \
-          "mkdir -p ${FOLDER}\n" \
-          "cp ~/CubismUP_3D/bin/simulation ${FOLDER}/\n" \
-          "export OMP_NUM_THREADS=12\n" \
-          "cd $FOLDER\n" \
-          "mpirun -np 1 ./simulation %s\n" \
-          % (runspec(nu, eps, cs, run), getSettings(nu, eps, cs))
-    subprocess.run(cmd, executable=parsed.shell, shell=True)
+def launchEuler(tpath, nu, eps, re, cs, run):
+    scalname = "%s/scalars_RE%03d" % (tpath, re)
+    logEname = "%s/spectrumLogE_RE%03d" % (tpath, re)
+    iCovname = "%s/invCovLogE_RE%03d" % (tpath, re)
+    runname  = runspec(re, cs, run)
+    cmd = "export LD_LIBRARY_PATH=/cluster/home/novatig/hdf5-1.10.1/gcc_6.3.0_openmpi_2.1/lib/:$LD_LIBRARY_PATH\n" \
+      "FOLDER=/cluster/scratch/novatig/CubismUP_3D/%s\n " \
+      "mkdir -p ${FOLDER}\n" \
+      "cp ~/CubismUP_3D/bin/simulation ${FOLDER}/\n" \
+      "cp %s ${FOLDER}/scalars_target\n" \
+      "cp %s ${FOLDER}/spectrumLogE_target\n" \
+      "cp %s ${FOLDER}/invCovLogE_target\n" \
+      "export OMP_NUM_THREADS=18\n" \
+      "cd $FOLDER\n" \
+      "bsub -n 18 -J %s -W 04:00 -R \"select[model==XeonGold_6150] span[ptile=18]\" mpirun -n 1 ./simulation %s\n" \
+      % (runname, scalname, logEname, iCovname, runname, getSettings(nu, eps, cs))
+    subprocess.run(cmd, shell=True)
 
 
 def launchDaint(nCases, les):
@@ -90,13 +99,11 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    NUs, EPSs = findAllParams(args.path)
-    nNus, nEps, nCss = len(NUs), len(EPSs), 9
-    LESs = np.linspace(0.16, 0.24, nCss)
+    nCss = 13
 
-    for ni in range(nNus):
-        for ei in range(nEps):
-            for li in range(nCss):
-                for ri in [0, 1, 2, 3, 4]:
-                    launchEuler(NUS[ni], EPS[ei], LESs[li], ri)
+    for re in np.linspace(60, 240, 19):
+      for cs in np.linspace(0.0, 0.24, nCss):
+        for ri in [3]:
+          eps, nu = epsNuFromRe(re)
+          launchEuler(args.path, nu, eps, re, cs, ri)
 
