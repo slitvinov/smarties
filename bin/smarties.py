577b20cc1617df1631d06fcbb1e9567b3095c252
novatig <novatig@ethz.ch>
2020-02-19 19:22:01 +0100

    minor

--- bin/smarties.py
+++ bin/smarties.py
@@ -388,6 +388,7 @@ if __name__ == '__main__':
   cmd = 'cd ${RUNDIR} \n'
   cmd = cmd + setEnvironmentFlags(parsed)
   cmd = cmd + setLaunchCommand(parsed, absRunPath)
+
   # print('COMMAND:' + cmd )
   signal.signal(signal.SIGINT, signal_handler)
   subprocess.run(cmd, executable=parsed.shell, shell=True)
