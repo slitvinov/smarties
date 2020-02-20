0243398ef8bd0307e0244df5722aa978a731d640
Guido Novati <novatig@ethz.ch>
2020-02-20 17:07:26 +0100

    RL blowfish

--- apps/CUP2D_blowfish/setup.sh
+++ apps/CUP2D_blowfish/setup.sh
@@ -8,16 +8,16 @@ fi
 cp ${COMPILEDIR}/blowfish ${RUNDIR}/exec
 
 # copy simulation settings files:
-cp runArguments* ${RUNDIR}/
+# (8 * 32)^2 grid
+# Re ~ sqrt(3*pi * radius / deltaRho / abs(gravity) / 8)
+cat <<EOF >${RUNDIR}/runArguments00.sh
+-poissonType cosine -muteAll 1 -bpdx 6 -bpdy 6 -tdump 0 -nu 0.001824116 \
+-tend 0 -shapes 'blowfish radius=0.2 bFixed=1'
+EOF
 
 # command line args to find app-required settings, each to be used for fixed
 # number of steps so as to increase sim fidelity as training progresses
-export EXTRA_LINE_ARGS=" --nStepPappSett 262144,262144,262144,0 \
---appSettings runArguments00.sh,runArguments01.sh,runArguments02.sh,runArguments03.sh "
+export EXTRA_LINE_ARGS=" --appSettings runArguments00.sh "
 
 # heavy application, needs dedicated processes
 export MPI_RANKS_PER_ENV=1
-
-#SETTINGS+=" --nStepPappSett 2097152,1048576,524288,0 "
-#export LD_LIBRARY_PATH=/cluster/home/novatig/VTK-7.1.0/Build/lib/:$LD_LIBRARY_PATH
-#cp ${HOME}/CubismUP_2D/makefiles/blowfish ${BASEPATH}${RUNFOLDER}/
