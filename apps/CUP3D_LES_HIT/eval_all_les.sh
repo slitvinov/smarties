b03b5ade449ce60f3171f1d2941161eceacf0e7d
Guido Novati <novatig@gmail.com>
2020-03-02 12:03:45 +0100

    minor

--- apps/CUP3D_LES_HIT/eval_all_les.sh
+++ apps/CUP3D_LES_HIT/eval_all_les.sh
@@ -6,7 +6,8 @@ export LES_RL_NBLOCK=${LES_RL_NBLOCK:-2}
 
 THISDIR=${SMARTIES_ROOT}/apps/CUP3D_LES_HIT
 
-for GRIDAGENT in 0 1 ; do
+for REW in LL GERMANO ; do
+for GRIDAGENT in 0 ; do
 for ACT in 2 4 8 16 ; do
 for RE in RE065 RE076 RE088 RE103 RE120 RE140 RE163 ; do
 
@@ -14,7 +15,7 @@ export LES_RL_EVALUATE=${RE}
 export LES_RL_FREQ_A=${ACT}
 export LES_RL_GRIDACT=1 #${GRIDAGENT}
 
-SPEC=${LES_RL_NETTYPE}_${LES_RL_NBLOCK}blocks_act`printf %02d $LES_RL_FREQ_A`
+SPEC=${REW}_${LES_RL_NETTYPE}_${LES_RL_NBLOCK}blocks_act`printf %02d $LES_RL_FREQ_A`
 
 if [ ${GRIDAGENT} == 0 ] ; then
 BASENAME=GridAgents_BlockRestart
@@ -26,13 +27,14 @@ RESTART=GridAgent
 export LES_RL_GRIDACTSETTINGS=1
 fi
 
-RUNDIR=${BASENAME}_RK2ND_${SPEC}_sim${LES_RL_N_TSIM}_${RE}
+RUNDIR=${BASENAME}_00_${SPEC}_sim${LES_RL_N_TSIM}_${RE}
 RESTARTDIR=${THISDIR}/trained_${RESTART}_${SPEC}/
 echo $RUNDIR
 
 smarties.py CUP3D_LES_HIT -n 1 -r ${RUNDIR} --restart ${RESTARTDIR} --nEvalSeqs 1
 #--printAppStdout
 
+done
 done
 done
 done
