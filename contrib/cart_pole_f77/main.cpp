71ce6dea5feb3af7cd81dcb2b3f12d0727414798
Sergey Litvinov <slitvinov@gmail.com>
2020-10-01 16:06:49 +0200

    no iso_c_binding

--- contrib/cart_pole_f77/main.cpp
+++ contrib/cart_pole_f77/main.cpp
@@ -1,11 +1,11 @@
 #include "smarties.h"
 
-extern "C" void app_main(const void *smarties_comm, const int f_mpicomm);
+extern "C" void app_main_(void *smarties_comm, int f_mpicomm);
 static int app_main_interface(smarties::Communicator *const smarties_comm,
                               MPI_Comm c_mpicomm, int argc, char **argv) {
   MPI_Fint f_mpicomm;
   f_mpicomm = MPI_Comm_c2f(c_mpicomm);
-  app_main(smarties_comm, f_mpicomm);
+  app_main_(smarties_comm, f_mpicomm);
   return 0;
 }
 
