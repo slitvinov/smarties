64919c6958ccefed8a5e2730b59afb84d70a7ff4
Sergey Litvinov <slitvinov@gmail.com>
2020-10-05 20:27:14 +0200

    wip: turbChannel/b

--- contrib/turbChannel/b/app_main.f
+++ contrib/turbChannel/b/app_main.f
@@ -11,26 +11,28 @@
       integer rank
       logical bounded
       integer status
+      integer i
 
       smarties_comm = comm
       status = system("sh -c $HOME/setup.sh")
-      write(6,*) 'Fortran side begins'
-      call smarties_setstateactiondims(comm, STATE_SIZE, NUM_ACTIONS,
+      call smarties_setStateActionDims(comm, STATE_SIZE, NUM_ACTIONS,
      +     AGENT_ID)
       bounded = .true.
-      call smarties_setactionscales(comm,
-     +      upper_action, lower_action,
-     +      bounded, NUM_ACTIONS, AGENT_ID)
+      call smarties_setActionScales(comm,
+     +     upper_action, lower_action,
+     +     bounded, NUM_ACTIONS, AGENT_ID)
       call smarties_setStateObservable(comm, b_observable, STATE_SIZE,
      +     AGENT_ID)
       call smarties_setStateScales(comm, upper_state, lower_state,
      +     STATE_SIZE, AGENT_ID)
-      call nek_init(mpicomm)
-      call nek_solve()
-      call smarties_sendTermState(comm, state, STATE_SIZE,
-     +     reward, AGENT_ID)
+      do while (.true.)
+         call smarties_sendInitState(smarties_comm, state,
+     +        STATE_SIZE, AGENT_ID)
+         call nek_init(mpicomm)
+         call nek_solve()
+         call smarties_sendTermState(comm, state, STATE_SIZE,
+     +        reward, AGENT_ID)
+      end do
       call nek_end()
-
-      write(6,*) 'Fortran side ends'
       app_main = 0
       end
