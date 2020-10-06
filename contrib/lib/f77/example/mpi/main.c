d1db0c48f62d0ed5ddd6e47631c0f5494d4f4ec7
Sergey Litvinov <slitvinov@gmail.com>
2020-10-06 19:42:37 +0200

    add examples

---
+++ contrib/lib/f77/example/mpi/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <mpi.h>
+#include <smarties_f77.h>
+
+static int
+main0(uintptr_t *smarties, void *mpi0, void *p)
+{
+    int flag;
+    FILE *file;
+    int rank;
+    int size;
+    MPI_Comm *mpi;
+
+    mpi = (MPI_Comm*)mpi0;
+    file = fopen("/dev/tty", "w");
+    if (MPI_Initialized(&flag) != 0) {
+        fprintf(stderr, "%s:%d: MPI_Initialized failed\n", __FILE__, __LINE__);
+        return 1;
+    }
+    if (flag)
+        fprintf(file, "mpi is initialized\n");
+    else
+        return 1;
+    if (MPI_Comm_rank(*mpi, &rank) != 0) {
+        fprintf(stderr, "%s:%d: MPI_Comm_rank failed\n", __FILE__, __LINE__);
+        return 1;
+    }
+    if (MPI_Comm_size(*mpi, &size) != 0) {
+        fprintf(stderr, "%s:%d: MPI_Comm_size failed\n", __FILE__, __LINE__);
+        return 1;
+    }
+    fprintf(file, "rank/size: %d/%d\n", rank, size);
+    return 0;
+}
+
+int
+main(int argc, char **argv)
+{
+    int i;
+
+    i = 42;
+    smarties_main(argc, argv, main0, &i);
+}
---
+++ contrib/lib/f77/example/serial/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <smarties_f77.h>
+
+static int
+main0(uintptr_t *smarties, void *mpi, void *p)
+{
+    int *i;
+    FILE *file;
+
+    i = p;
+    file = fopen("/dev/tty", "w");
+    fprintf(file, "magic: %d\n", *i);
+    return 0;
+}
+
+int
+main(int argc, char **argv)
+{
+    int i;
+    i = 42;
+    smarties_main(argc, argv, main0, &i);
+}
