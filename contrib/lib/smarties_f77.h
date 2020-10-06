1f0a4b170c0cee1de80d27ab171fec9fcd9b6a51
Sergey Litvinov <slitvinov@gmail.com>
2020-10-06 11:53:57 +0200

    add smarties_f77.h

---
+++ contrib/lib/smarties_f77.h
@@ -0,0 +1,8 @@
+void smarties_sendinitstate_ (uintptr_t, double *S, int *dim, int *);
+void smarties_sendtermstate_ (uintptr_t, double *S, int *dim, double *R, int *);
+void smarties_sendstate_ (uintptr_t, double *S, int *dim, double *R, int *);
+void smarties_recvaction_ (uintptr_t, double *A, int *dim, int *);
+void smarties_setactionscales_ (uintptr_t, double *hi, double *lo, int *are_bounds, int *dim, int *);
+void smarties_setstateobservable_ (uintptr_t, int *bobs, int *dim, int *);
+void smarties_setstatescales_ (uintptr_t, double *hi, double *lo, int *dim, int *);
+void smarties_setstateactiondims_ (uintptr_t *, int *dim, int *dim, int *);
