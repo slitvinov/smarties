521e6ad4a200a9d60d7dbfcd9c7e8ee46369e82b
Sergey Litvinov <slitvinov@gmail.com>
2020-10-10 15:56:22 +0200

    dlopen: wip

--- contrib/example/dlopen/main.c
+++ contrib/example/dlopen/main.c
@@ -1,66 +1,46 @@
+#include <dlfcn.h>
+#include <mpi.h>
 #include <stdint.h>
 #include <stdio.h>
-#include <mpi.h>
-#include <dlfcn.h>
 #include <smarties_f77.h>
-
-void *so;
-static void*
-sym(void)
-{
-    void *function;
-    if ((so = dlopen(NULL, RTLD_LAZY | RTLD_LOCAL | RTLD_DEEPBIND)) == NULL) {
-      fprintf(stderr, "%s:%d: dlopen failed: %s\n", __FILE__, __LINE__, dlerror());
-      return NULL;
-    }
-    if ((function = dlsym(so, "app_main_")) == NULL) {
-      fprintf(stderr, "%s:%d: dlsym failed: %s\n", __FILE__, __LINE__, dlerror());
-      return NULL;
-    }
-    return function;
-}
-
 
 static int
 main0(uintptr_t *smarties, void *mpi0, void *p)
 {
-    typedef int (*Function)(uintptr_t*, int*, int*);
-    MPI_Fint f_mpicomm;
-    MPI_Comm *mpi;
-    int *i;
-    int first;
-    FILE *file;
-    Function function;
+  typedef int (*Function)(uintptr_t *, int *, int *);
+  MPI_Fint f_mpicomm;
+  MPI_Comm *mpi;
+  int first;
+  FILE *file;
+  Function function;
+  void *lib;
 
-    mpi = (MPI_Comm*)mpi0;
-    i = (int*)p;
-    fprintf(stderr, "main0: %d\n", *i);
-    f_mpicomm = MPI_Comm_c2f(*mpi);
-
-    function = (Function)sym();
-    first = 1;
+  mpi = (MPI_Comm *)mpi0;
+  f_mpicomm = MPI_Comm_c2f(*mpi);
+  for (first = 1;; first = 0) {
+    if ((lib = dlopen("libmain.so", RTLD_LAZY | RTLD_LOCAL | RTLD_DEEPBIND)) == NULL) {
+      fprintf(stderr, "%s:%d: dlopen failed: %s\n", __FILE__, __LINE__,
+              dlerror());
+      return 1;
+    }
+    if ((function = (Function)dlsym(lib, "app_main_")) == NULL) {
+      fprintf(stderr, "%s:%d: dlsym failed: %s\n", __FILE__, __LINE__,
+              dlerror());
+      return 1;
+    }
     function(smarties, &f_mpicomm, &first);
-    if (dlclose(so) != 0) {
-      fprintf(stderr, "%s:%d: dlclose failed: %s\n", __FILE__, __LINE__, dlerror());
-      return 2;
+    if (dlclose(lib) != 0) {
+      fprintf(stderr, "%s:%d: dlclose failed: %s\n", __FILE__, __LINE__,
+              dlerror());
+      return 1;
     }
-    for (;;) {
-      function = (Function)sym();
-      first = 0;
-      function(smarties, &f_mpicomm, &first);
-      if (dlclose(so) != 0) {
-        fprintf(stderr, "%s:%d: dlclose failed: %s\n", __FILE__, __LINE__, dlerror());
-        return 2;
-      }
-    }
-    return 0;
+  }
+  return 0;
 }
 
 int
 main(int argc, char **argv)
 {
-    int i;
-
-    i = 1234;
-    return smarties_main_(argc, argv, main0, &i);
+  int i;
+  return smarties_main_(argc, argv, main0, &i);
 }
--- contrib/install.sh
+++ contrib/install.sh
@@ -1,112 +1,14 @@
 #!/bin/sh
 
 : ${PREFIX=$HOME/.local}
-me=contirb/install.sh
 
-err () {
-    printf >&2 %s\\n "$me: $*"
-    exit 2
-}
+FCFLAGS='-Ofast -g'
+CFLAGS='-Ofast -g -fPIC'
+CXXFLAGS='-Ofast -g -fno-gnu-unique -fPIC'
+MAKEFLAGS=-e
 
-: ${CMAKEFLAGS=-DCOMPILE_PY_SO=NO}
-
-# find source/smarties -name '*.h' | sort
-H='
-include/smarties_extern.h
-include/smarties.h
-source/smarties/Communicator.h
-source/smarties/Core/Agent.h
-source/smarties/Core/Environment.h
-source/smarties/Core/Launcher.h
-source/smarties/Core/Master.h
-source/smarties/Core/StateAction.h
-source/smarties/Core/Worker.h
-source/smarties/Engine.h
-source/smarties/Learners/ACER.h
-source/smarties/Learners/AlgoFactory.h
-source/smarties/Learners/CMALearner.h
-source/smarties/Learners/DPG.h
-source/smarties/Learners/DQN.h
-source/smarties/Learners/Learner_approximator.h
-source/smarties/Learners/Learner.h
-source/smarties/Learners/Learner_pytorch.h
-source/smarties/Learners/MixedPG.h
-source/smarties/Learners/NAF.h
-source/smarties/Learners/PPO.h
-source/smarties/Learners/RACER.h
-source/smarties/Math/Continuous_policy.h
-source/smarties/Math/Discrete_advantage.h
-source/smarties/Math/Discrete_policy.h
-source/smarties/Math/Gaus_advantage.h
-source/smarties/Math/Quadratic_advantage.h
-source/smarties/Math/Quadratic_term.h
-source/smarties/Math/Zero_advantage.h
-source/smarties/Network/Approximator.h
-source/smarties/Network/Builder.h
-source/smarties/Network/CMA_Optimizer.h
-source/smarties/Network/Conv2Dfactory.h
-source/smarties/Network/Layers/Activation.h
-source/smarties/Network/Layers/Functions.h
-source/smarties/Network/Layers/Layer_Base.h
-source/smarties/Network/Layers/Layer_Conv2D.h
-source/smarties/Network/Layers/Layer_GRU.h
-source/smarties/Network/Layers/Layer_LSTM.h
-source/smarties/Network/Layers/Layers.h
-source/smarties/Network/Layers/Parameters.h
-source/smarties/Network/Network.h
-source/smarties/Network/Optimizer.h
-source/smarties/Network/ThreadContext.h
-source/smarties/ReplayMemory/DataCoordinator.h
-source/smarties/ReplayMemory/Episode.h
-source/smarties/ReplayMemory/ExperienceRemovalAlgorithms.h
-source/smarties/ReplayMemory/MemoryBuffer.h
-source/smarties/ReplayMemory/MemoryProcessing.h
-source/smarties/ReplayMemory/MiniBatch.h
-source/smarties/ReplayMemory/ReplayStatsCounters.h
-source/smarties/ReplayMemory/Sampling.h
-source/smarties/Settings/Bund.h
-source/smarties/Settings/Definitions.h
-source/smarties/Settings/ExecutionInfo.h
-source/smarties/Settings/HyperParameters.h
-source/smarties/Utils/DelayedReductor.h
-source/smarties/Utils/FunctionUtilities.h
-source/smarties/Utils/LauncherUtilities.h
-source/smarties/Utils/MPIUtilities.h
-source/smarties/Utils/ParameterBlob.h
-source/smarties/Utils/Profiler.h
-source/smarties/Utils/SocketsLib.h
-source/smarties/Utils/SstreamUtilities.h
-source/smarties/Utils/StatsTracker.h
-source/smarties/Utils/TaskQueue.h
-source/smarties/Utils/ThreadSafeVec.h
-source/smarties/Utils/Warnings.h
-'
-# find include source/smarties -name '*.h' | xargs -n1 dirname | sort | uniq
-D='
-include
-source/smarties
-source/smarties/Core
-source/smarties/Learners
-source/smarties/Math
-source/smarties/Network
-source/smarties/Network/Layers
-source/smarties/ReplayMemory
-source/smarties/Settings
-source/smarties/Utils
-'
-mkdir -p build
-(cd build
- cmake .. $CMAKEFLAGS || err 'cmake failed'
- make || err 'make failed'
-) || exit 2
-
-mkdir -p "$PREFIX"/include "$PREFIX"/lib
-cp include/smarties.f90 "$PREFIX"/include/smarties.f90 || err 'fail to f90 interface'
-cp lib/libsmarties.so "$PREFIX"/lib/libsmarties.so || err 'fail to install library'
-
-for i in $D
-do mkdir -p "$PREFIX"/$i
-done
-for i in $H
-do cp $i "$PREFIX"/$i
-done
+export FCFLAGS CFLAGS CXXFLAGS MAKEFLAGS
+make install &&
+(cd contrib/lib/f77 && make install) &&
+(cd contrib/example/dlopen/lib && make) &&
+(cd contrib/example/dlopen && make)
