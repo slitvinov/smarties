3eb6c78bb8130f34fb523f4e78b909816855591d
Sergey Litvinov <slitvinov@gmail.com>
2020-10-11 19:48:50 +0200

    smarties_f77: wip

--- contrib/lib/f77/smarties_f77.h
+++ contrib/lib/f77/smarties_f77.h
@@ -13,7 +13,7 @@ void smarties_setstateobservable_(uintptr_t *, int *bobs, int *dim, int *);
 void smarties_setstatescales_(uintptr_t *, double *hi, double *lo, int *dim,
 			      int *);
 void smarties_setstateactiondims_(uintptr_t *, int *sdim, int *adim, int *);
-int smarties_main0_(int argc, char **, int nwpe, int (*f)(uintptr_t*, void *mpi, void*), void *);  
+int smarties_main0_(int argc, char **, int nwpe, int (*f)(uintptr_t*, void *mpi, void*), void *);
 int smarties_main_(int argc, char **, int (*f)(uintptr_t*, void *mpi, void*), void *);
 int smarties_dlopen_(int argc, char **argv, const char *filename, void *data);
 
