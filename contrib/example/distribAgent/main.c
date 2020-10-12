ac319517adfb3b5f953846edf7aa6f8effccd5f8
Sergey Litvinov <slitvinov@gmail.com>
2020-10-12 15:32:17 +0200

    minor

--- contrib/example/distribAgent/main.c
+++ contrib/example/distribAgent/main.c
@@ -13,7 +13,8 @@ const double g = 9.81;
 const double dt = 4e-4;
 const int nsteps = 50;
 int step = 0;
-double F = 0, t = 0;
+double F = 0;
+double t = 0;
 static double u[4];
 int action_dim = NCARTS;
 int state_dim = 4 * NCARTS;
@@ -33,11 +34,18 @@ rnd(void)
 }
 
 void Diff(double *_u, double *res) {
-  const double cosy = cos(_u[2]), siny = sin(_u[2]);
-  const double w = _u[3];
-  const double totMass = mp + mc;
-  const double fac2 = l * (4.0 / 3 - (mp * cosy * cosy) / totMass);
-  const double F1 = F + mp * l * w * w * siny;
+  double cosy;
+  double siny;
+  double w;
+  double totMass;
+  double fac2;
+  double F1;
+  cosy = cos(_u[2]);
+  siny = sin(_u[2]);
+  w = _u[3];
+  totMass = mp + mc;
+  fac2 = l * (4.0 / 3 - (mp * cosy * cosy) / totMass);
+  F1 = F + mp * l * w * w * siny;
   res[3] = (g * siny - F1 * cosy / totMass) / fac2;
   res[1] = (F1 - mp * l * res[3] * cosy) / totMass;
   res[0] = _u[1];
@@ -45,22 +53,20 @@ void Diff(double *_u, double *res) {
 }
 
 void rk46_nl(double t0, double dt, double *u0) {
-  static double a[] = {0.000000000000,  -0.737101392796, -1.634740794341,
+  const double a[] = {0.000000000000,  -0.737101392796, -1.634740794341,
                        -0.744739003780, -1.469897351522, -2.813971388035};
-  static double b[] = {0.032918605146, 0.823256998200, 0.381530948900,
+  const double b[] = {0.032918605146, 0.823256998200, 0.381530948900,
                        0.200092213184, 1.718581042715, 0.270000000000};
-  static double c[] = {0.000000000000, 0.032918605146, 0.249351723343,
+  const double c[] = {0.000000000000, 0.032918605146, 0.249351723343,
                        0.466911705055, 0.582030414044, 0.847252983783};
-  int s = 6;
   double w[4];
   double res[4];
   double u[4];
   int i;
   int d;
-
   for (d = 0; d < 4; d++)
     u[d] = u0[d];
-  for (i = 0; i < s; ++i) {
+  for (i = 0; i < 6; ++i) {
     t = t0 + dt * c[i];
     Diff(u, res);
     for (d = 0; d < 4; d++) {
@@ -72,7 +78,8 @@ void rk46_nl(double t0, double dt, double *u0) {
     u0[d] = u[d];
 }
 
-void reset() {
+static void
+reset(void) {
   int d;
   for (d = 0; d < 4; d++)
     u[d] = 0.05*rnd();
@@ -81,12 +88,19 @@ void reset() {
   t = 0;
 }
 
-int is_failed() { return fabs(u[0]) > 2.4 || fabs(u[2]) > M_PI / 15; }
-int is_over() {
+static int
+is_failed(void) {
+  return fabs(u[0]) > 2.4 || fabs(u[2]) > M_PI / 15;
+}
+
+static int
+is_over(void) {
   return step >= 500 || fabs(u[0]) > 2.4 || fabs(u[2]) > M_PI / 15;
 }
 
-int advance(double action) {
+static int
+advance(double action)
+{
   F = action;
   step++;
   for (int i = 0; i < nsteps; i++) {
@@ -98,21 +112,37 @@ int advance(double action) {
   return 0;
 }
 
-void getState(double *state) {
+static void
+getState(double *state)
+{
   int d;
   for (d = 0; d < 4; d++)
     state[d] = u[d];
 }
 
-double getReward() { return 1 - (fabs(u[2]) > M_PI / 15 || fabs(u[0]) > 2.4); }
+static double
+getReward(void) {
+  return 1 - (fabs(u[2]) > M_PI / 15 || fabs(u[0]) > 2.4);
+}
 
 static int
 main0(uintptr_t *smarties0, void *mpi0, void *p)
 {
   int d;
   double myState[4];
-  int myRank, simSize;
+  int myRank;
+  int simSize;
   MPI_Comm mpicom;
+  int bounded;
+  double upper_action_bound[NCARTS] = {10, 10};
+  double lower_action_bound[NCARTS] = {-10, -10};
+  double combinedState[4 * NCARTS];
+  double combinedAction[action_dim];
+  double sumReward;
+  double myAction;
+  double myReward;
+  int nTerminated;
+  int myTerminated;
 
   mpicom = *(MPI_Comm*)mpi0;
   smarties = *smarties0;
@@ -129,40 +159,31 @@ main0(uintptr_t *smarties0, void *mpi0, void *p)
   //  comm->setStateActionDims(4 * NCARTS, 1 * NCARTS);
 
   // OPTIONAL: action bounds
-  int bounded = 1;
-  double upper_action_bound[NCARTS] = {10, 10};
-  double lower_action_bound[NCARTS] = {-10, -10};
+  bounded = 1;
   smarties_setactionscales_(&smarties, upper_action_bound, lower_action_bound, &bounded, &action_dim, &agent);
   MPI_Barrier(mpicom);
   while (1) // train loop
   {
-    {
-      // reset environment:
-      reset();
-      getState(myState);
-      double combinedState[4 * NCARTS];
-      for (d = 0; d < 4 * NCARTS; d++)
-	combinedState[d] = 0;
-      MPI_Allgather(myState, 4, MPI_DOUBLE, combinedState, 4,
-                    MPI_DOUBLE, mpicom);
-      // Actually, only rank 0 will send the state to smarties.
-      // We might as well have used MPI_Gather with root 0.
-      smarties_sendinitstate_(&smarties, combinedState, &state_dim, &agent);
-    }
-
+    reset();
+    getState(myState);
+    for (d = 0; d < 4 * NCARTS; d++)
+      combinedState[d] = 0;
+    MPI_Allgather(myState, 4, MPI_DOUBLE, combinedState, 4,
+		  MPI_DOUBLE, mpicom);
+    // Actually, only rank 0 will send the state to smarties.
+    // We might as well have used MPI_Gather with root 0.
+    smarties_sendinitstate_(&smarties, combinedState, &state_dim, &agent);
     while (1) // simulation loop
     {
       // Each rank will get the same vector here:
-      double combinedAction[action_dim];
       smarties_recvaction_(&smarties, combinedAction, &action_dim, &agent);
-      double myAction = combinedAction[myRank];
-      const int myTerminated = advance(myAction);
+      myAction = combinedAction[myRank];
+      myTerminated = advance(myAction);
       getState(myState);
-      const double myReward = getReward();
+      myReward = getReward();
 
-      double combinedState[4 * NCARTS];
-      double sumReward = 0;
-      int nTerminated = 0;
+      sumReward = 0;
+      nTerminated = 0;
 
       MPI_Allreduce(&myTerminated, &nTerminated, 1, MPI_INT, MPI_SUM, mpicom);
       MPI_Allreduce(&myReward, &sumReward, 1, MPI_DOUBLE, MPI_SUM, mpicom);
