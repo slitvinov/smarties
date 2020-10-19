64a77a4a97922bdce33dec0576f24a3425379592
Sergey Litvinov <slitvinov@gmail.com>
2020-10-19 15:26:33 +0200

    clean up; use ap.format

--- contrib/example/f77/main.c
+++ contrib/example/f77/main.c
@@ -1,29 +1,19 @@
-#include <stdint.h>
-#include <stdio.h>
 #include <mpi.h>
 #include <smarties_f77.h>
+#include <stdint.h>
+#include <stdio.h>
 
-extern int app_main_(uintptr_t*, int*);
-static int
-main0(uintptr_t *smarties, void *mpi0, void *p)
-{
-    MPI_Fint f_mpicomm;
-    MPI_Comm *mpi;
-    int *i;
-    FILE *file;
+extern int app_main_(uintptr_t *, int *);
+static int main0(uintptr_t *smarties, void *mpi0, void *p) {
+  MPI_Fint f_mpicomm;
+  MPI_Comm *mpi;
+  FILE *file;
 
-    mpi = (MPI_Comm*)mpi0;
-    i = (int*)p;
-    fprintf(stderr, "main0: %d\n", *i);
-    f_mpicomm = MPI_Comm_c2f(*mpi);
-    return app_main_(smarties, &f_mpicomm);
+  mpi = (MPI_Comm *)mpi0;
+  f_mpicomm = MPI_Comm_c2f(*mpi);
+  return app_main_(smarties, &f_mpicomm);
 }
 
-int
-main(int argc, char **argv)
-{
-    int i;
-
-    i = 1234;
-    return smarties_main_(argc, argv, main0, &i);
+int main(int argc, char **argv) {
+  return smarties_main_(argc, argv, main0, NULL);
 }
--- contrib/example/f77/size/cpp.cpp
+++ contrib/example/f77/size/cpp.cpp
@@ -1,8 +1,7 @@
-#include <stdio.h>
 #include <cstdint>
+#include <stdio.h>
 
-int main()
-{
+int main() {
   fprintf(stderr, "integer %d\n", sizeof(int));
   fprintf(stderr, "double %d\n", sizeof(double));
   fprintf(stderr, "pointer %d\n", sizeof(uintptr_t));
