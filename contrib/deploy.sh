c1da37f8141b269cf34f9580541a0ce80092fb1d
Sergey Litvinov <slitvinov@gmail.com>
2020-10-21 10:58:41 +0200

    contrib/deploy.sh: use -O2

--- contrib/deploy.sh
+++ contrib/deploy.sh
@@ -3,12 +3,12 @@
 set -e
 cd
 git clone --depth 1 --recursive git@github.com:Nek5000/Nek5000.git
-git clone --depth 1 -b f77 git@github.com:slitvinov/smarties
+git clone -b f77 git@github.com:slitvinov/smarties
 cd smarties
 . contrib/smarties.env
 ./contrib/install.sh
 cd contrib/turbChannel/dlopen
-MPI=0 FFLAGS='-Ofast -g -fPIC' CFLAGS='-Ofast -g -fPIC' ~/Nek5000/bin/nekconfig  -build-dep
+MPI=0 FFLAGS='-O2 -g -fPIC' CFLAGS='-O2 -g -fPIC' ~/Nek5000/bin/nekconfig  -build-dep
 MPI=0 ~/Nek5000/bin/nekconfig
 make -f make/lib.mk -j
 make -f make/bin.mk
